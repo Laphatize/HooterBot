ae29439a6e29dc4c4e307affe98860a09fb2a3fd
Liam Chambers <leemleem1234@outlook.com>
2021-07-05 15:47:35 -0400

    DJSversion in bot info formatting update
    Trying to split developer version on new line

--- COMMANDS/botinfo.js
+++ COMMANDS/botinfo.js
@@ -24,6 +24,17 @@ module.exports = {
 
         let botUptime = `${days}D, ${hours}H, ${minutes}M and ${seconds}S`;
         
+        let DJSv = pjson.dependencies['discord.js'].split(`^`).pop().split(',', 1)[0]
+
+        if(pjson.dependencies['discord.js'].split(`^`).pop().includes('dev')) {
+            
+            devVer = pjson.dependencies['discord.js'].split(`-`).pop()
+            DJSversion = DJSv.concat(`\n${devVer}`)
+        }
+        else {
+            DJSversion = DJSv;
+        }
+
         // CREATING EMBED FOR RESPONSE        
         let infoEmbed = new discord.MessageEmbed()
         .setColor(config.embedTempleRed)
@@ -32,7 +43,7 @@ module.exports = {
         .addField(`Bot Version:`, `**${pjson.version}**`, true)
         .addField(`Build Date:`, `${config.buildDate}`, true)
         .addField(`${config.emjNodejs} NodeJS:`, `${process.version.split(`v`).pop()}`, true)
-        .addField(`${config.emjDiscordjs} Discord.js:`, `${pjson.dependencies['discord.js'].split(`^`).pop().split(`-`)}`, true)
+        .addField(`${config.emjDiscordjs} Discord.js:`, `${DJSversion}`, true)
         .addField(`Uptime:`, `${botUptime}`, true)
         .addField(`Description:`, `*"${pjson.description}"*`)
         .addField(`GitHub Repository`, `${pjson.repository.url.split(`+`).pop()}`)
