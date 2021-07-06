364e8d2ac676ae4e6df41096d235c4bf338f49de
Liam Chambers <leemleem1234@outlook.com>
2021-07-06 14:55:00 -0400

    updateCache structure fix

--- events/updateCache.js
+++ events/updateCache.js
@@ -3,19 +3,16 @@ const guildPrefixes = {}
 
 
 // CONNECT TO DB
-module.exports = {
-    name: 'loadPrefixes',
-    async execute(client) {
-        for (const guild of client.guilds.cache) {
-            const guildID = guild[1].id
+module.exports.loadPrefixes = async (client) => {
+    for (const guild of client.guilds.cache) {
+        const guildID = guild[1].id
 
-            const result = await guildSchema.findOne({ GUILD_ID: guildID })
-            try {
-                guildPrefixes[guildID] = result.prefix
-            }
-            catch(error) {
-                // THE SERVER DOES NOT HAVE A CUSTOM PREFIX, IGNORE.
-            }
+        const result = await guildSchema.findOne({ GUILD_ID: guildID })
+        try {
+            guildPrefixes[guildID] = result.prefix
+        }
+        catch(error) {
+            // THE SERVER DOES NOT HAVE A CUSTOM PREFIX, IGNORE.
         }
     }
 }
