fa7c3f481a707f10a7f26529a7ad71757555ef77
Liam Chambers <leemleem1234@outlook.com>
2021-07-07 18:04:59 -0400

    missing "guild"

--- botEvents/messageCreate.js
+++ botEvents/messageCreate.js
@@ -15,7 +15,7 @@ module.exports = {
 
 
         // CHECKING IF BOT HAS PERMISSION TO SPEAK IN THE CHANNEL
-        if (!message.channel.permissionsFor(guild.me)) { 
+        if (!message.channel.permissionsFor(message.guild.me)) { 
 
             console.log(`Bot cannot speak in the channel.`)
 
