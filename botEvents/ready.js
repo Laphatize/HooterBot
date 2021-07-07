35c6520c3196a31971915e46ace54a313aeaaa53
Liam Chambers <leemleem1234@outlook.com>
2021-07-07 01:17:55 -0400

    logging formatting improvement

--- botEvents/ready.js
+++ botEvents/ready.js
@@ -8,29 +8,29 @@ module.exports = {
 	async execute(client) {
 
 		// LOGGING IN
-		console.log(`\n\n========= GOING ONLINE =========`);
+		console.log(`\n\n============ GOING ONLINE ============`);
 		console.log(`${config.botName} has logged in successfully.`);
-		console.log(`================================\n\n`);
+		console.log(`======================================\n\n`);
 
 	
 		// SETTING ACTIVITY STATUS
-		console.log(`======= ACTIVITY STATUS ========`);
+		console.log(`========== ACTIVITY STATUS ===========`);
 		client.user.setActivity(`verification requests`, {type: "WATCHING"});
 		console.log(`${config.botName}'s activity status is set.`)
-		console.log(`================================\n\n`);
+		console.log(`======================================\n\n`);
 
 
 		// LOADING PREFIXES FROM DB
-		console.log(`===== REGISTERING PREFIXES =====`);
+		console.log(`======== REGISTERING PREFIXES ========`);
 		updateCache.loadPrefixes(client);
 		console.log(`Prefixes registered.`);
-		console.log(`================================\n\n`);
+		console.log(`======================================\n\n`);
 
 
 		// CONNECTING TO MONGO DATABASE
 		await db()
-		console.log(`##### DATABASE CONNECTION ######`);
+		console.log(`######## DATABASE CONNECTION #########`);
 		console.log(`Mongo DB connection made.`);
-		console.log(`################################\n\n`);
+		console.log(`######################################\n\n`);
 	},
 };
