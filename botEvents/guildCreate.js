d54258cc1b321a0e3fcdd042826d82537970a847
Liam Chambers <leemleem1234@outlook.com>
2021-07-07 20:34:35 -0400

    guildJoin message fix and DB logging attempt

--- botEvents/guildCreate.js
+++ botEvents/guildCreate.js
@@ -5,17 +5,15 @@ const guildSchema = require('../Database/guildSchema');
 module.exports = {
 	name: 'guildCreate',
 	async execute(member, client) {
-      
-            const introduceYourselfChannel = client.channels.cache.get(config.introductionsChannelId)
 
             // DM EMBED MESSAGE
             const botJoinEmbed = new discord.MessageEmbed()
                 .setColor(config.embedTempleRed)
                 .setTitle(`👋 **Hello fellow owls!** ${config.emjTempleT}`)
-                .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthor}> to help out in the server. I like throwing long error messages at MrMusicMan789 and exploring new features on Discord. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet, right?), but I'm looking forward to hanging out with everyone in the server! :)\nIf you want to learn more about me and what I can do, head to <#${config.botchannelId}> and run my commands \`\`<PREFIX>botinfo.\`\` and \`\`<PREFIX>help\`\`.'`)
+                .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthorId}> to help out in the server (yell at him if I ever break). I like throwing long error messages at MrMusicMan789 and exploring new features on Discord. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet, right?), but I'm looking forward to hanging out with everyone in the server! :)\nIf you want to learn more about me and what I can do, head to <#${config.botchannelId}> and run my commands \`\`<PREFIX>botinfo.\`\` and \`\`<PREFIX>help\`\`.'`)
             
             // SEND TO #introduce-yourself CHANNEL
-            const message = await introduceYourselfChannel.send({embeds: [botJoinEmbed]})
+            const message = await config.introductionsChannelId.send({embeds: [botJoinEmbed]})
 
             // CREATE DATABASE ENTRY FOR GUILD
             const result = await guildSchema.findOne({
@@ -26,19 +24,17 @@ module.exports = {
             
             console.log(`result = ${result}`)
 
-            // if(!result){
-            //     await guildSchema.insertOne({
-            //         GUILD_NAME: message.guild.name,
-            //         GUILD_ID: message.guild.id,
-            //         REGISTERED: Date.now(),
-            //         PREFIX: config.prefix,
-            //         TICKET_CAT_ID: null,
-            //         VERIF_PROMPT_CH_ID: null,
-            //         VERIF_PROMPT_MSG_ID: null
-            //     },{ 
-            //         // IF DNE, CREATE ENTRY FOR GUILD
-            //         upsert: true
-            //     })
-            // }
+            if(!result.PREFIX == config.prefix){
+                await guildSchema.findOneAndUpdate({
+                    // CONTENT USED TO FIND UNIQUE ENTRY
+                    GUILD_NAME: message.guild.name,
+                    GUILD_ID: message.guild.id
+                },{
+                    // CONTENT TO BE UPDATED
+                    PREFIX: config.prefix
+                },{ 
+                    upsert: true
+                })
+            }
 	},
 };
