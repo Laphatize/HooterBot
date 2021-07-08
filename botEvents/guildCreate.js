dcfaf1075ff1610821b8a14ccecd63f18d45faa6
Liam Chambers <leemleem1234@outlook.com>
2021-07-07 20:38:32 -0400

    Improved logging

--- botEvents/guildCreate.js
+++ botEvents/guildCreate.js
@@ -6,35 +6,41 @@ module.exports = {
 	name: 'guildCreate',
 	async execute(member, client) {
 
-            // DM EMBED MESSAGE
-            const botJoinEmbed = new discord.MessageEmbed()
-                .setColor(config.embedTempleRed)
-                .setTitle(`👋 **Hello fellow owls!** ${config.emjTempleT}`)
-                .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthorId}> to help out in the server (yell at him if I ever break). I like throwing long error messages at MrMusicMan789 and exploring new features on Discord. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet, right?), but I'm looking forward to hanging out with everyone in the server! :)\nIf you want to learn more about me and what I can do, head to <#${config.botchannelId}> and run my commands \`\`<PREFIX>botinfo.\`\` and \`\`<PREFIX>help\`\`.'`)
-            
-            // SEND TO #introduce-yourself CHANNEL
-            const message = await config.introductionsChannelId.send({embeds: [botJoinEmbed]})
+        console.log(`======================================`)
+        console.log(`GUILD EVENT: NEW SERVER JOINED`)
+        console.log(`======================================`)
 
-            // CREATE DATABASE ENTRY FOR GUILD
-            const result = await guildSchema.findOne({
+        // DM EMBED MESSAGE
+        const botJoinEmbed = new discord.MessageEmbed()
+            .setColor(config.embedTempleRed)
+            .setTitle(`👋 **Hello fellow owls!** ${config.emjTempleT}`)
+            .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthorId}> to help out in the server (yell at him if I ever break). I like throwing long error messages at MrMusicMan789 and exploring new features on Discord. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet, right?), but I'm looking forward to hanging out with everyone in the server! :)\nIf you want to learn more about me and what I can do, head to <#${config.botchannelId}> and run my commands \`\`<PREFIX>botinfo.\`\` and \`\`<PREFIX>help\`\`.'`)
+        
+        // SEND TO #introduce-yourself CHANNEL
+        const message = await config.introductionsChannelId.send({embeds: [botJoinEmbed]})
+
+        // CREATE DATABASE ENTRY FOR GUILD
+        const result = await guildSchema.findOne({
+            // CONTENT USED TO FIND UNIQUE ENTRY
+            GUILD_NAME: message.guild.name,
+            GUILD_ID: message.guild.id
+        })
+        
+
+        if(!result.PREFIX == config.prefix){
+            await guildSchema.findOneAndUpdate({
                 // CONTENT USED TO FIND UNIQUE ENTRY
                 GUILD_NAME: message.guild.name,
                 GUILD_ID: message.guild.id
+            },{
+                // CONTENT TO BE UPDATED
+                PREFIX: config.prefix
+            },{ 
+                upsert: true
             })
-            
-            console.log(`result = ${result}`)
+        }
 
-            if(!result.PREFIX == config.prefix){
-                await guildSchema.findOneAndUpdate({
-                    // CONTENT USED TO FIND UNIQUE ENTRY
-                    GUILD_NAME: message.guild.name,
-                    GUILD_ID: message.guild.id
-                },{
-                    // CONTENT TO BE UPDATED
-                    PREFIX: config.prefix
-                },{ 
-                    upsert: true
-                })
-            }
+        console.log(result)
+        console.log(`======================================\n\n`)
 	},
 };
