346bc8bc30a26a6c2aa70e6f3180e974206beba7
Liam Chambers <leemleem1234@outlook.com>
2021-07-07 20:59:11 -0400

    Fixing welcome message posting

--- botEvents/guildCreate.js
+++ botEvents/guildCreate.js
@@ -10,14 +10,16 @@ module.exports = {
         console.log(`GUILD EVENT: NEW SERVER JOINED`)
         console.log(`======================================`)
 
+
         // DM EMBED MESSAGE
         const botJoinEmbed = new discord.MessageEmbed()
             .setColor(config.embedTempleRed)
             .setTitle(`👋 **Hello fellow owls!** ${config.emjTempleT}`)
             .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthorId}> to help out in the server (yell at him if I ever break). I like throwing long error messages at MrMusicMan789 and exploring new features on Discord. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet, right?), but I'm looking forward to hanging out with everyone in the server! :)\nIf you want to learn more about me and what I can do, head to <#${config.botchannelId}> and run my commands \`\`<PREFIX>botinfo.\`\` and \`\`<PREFIX>help\`\`.'`)
-        
+
+
         // SEND TO #introduce-yourself CHANNEL
-        const message = await config.introductionsChannelId.send({embeds: [botJoinEmbed]})
+        const message = await client.channels.cache.get(config.introductionsChannelId).send({embeds: [botJoinEmbed]})
 
 
 
