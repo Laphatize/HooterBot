a3b0c14c78dc5d8e29f49033440955b5472c7035
Liam Chambers <leemleem1234@outlook.com>
2021-07-07 22:05:09 -0400

    Welcome prompt verbiage tweak

--- botEvents/guildCreate.js
+++ botEvents/guildCreate.js
@@ -14,7 +14,7 @@ module.exports = {
         const botJoinEmbed = new discord.MessageEmbed()
             .setColor(config.embedTempleRed)
             .setTitle(`👋 **Hello fellow owls!** ${config.emjTempleT}`)
-            .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthorId}> to help out in the server *(please yell at him if I ever break)*. I like long walks on the beach and throwing long error messages at MrMusicMan789. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet?), but I'm looking forward to hanging out with everyone in the server.
+            .setDescription(`I'm ${config.botName}, a bot built by <@${config.botAuthorId}> to help out in the server *(please yell at him if I ever break)*. I like long walks on the beach and throwing long callstacks at MrMusicMan789 when he makes silly programming mistakes. I don't know what I'm going to study at Temple since I've not been given machine learning code (...yet?), but I'm looking forward to hanging out with everyone in the server.
             \nIf you want to learn more about me and what I can do, head to <#${config.botchannelId}> and run my commands \`\`${config.prefix}botinfo\`\` and \`\`${config.prefix}help\`\`.'`)
 
 
