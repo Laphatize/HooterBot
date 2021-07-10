6ee0fc52d09ca62098cd5a9ec6d8e4d9d1ce25c9
Liam Chambers <leemleem1234@outlook.com>
2021-07-10 15:04:38 -0400

    Commenting logic plans, testing ephemeral reply.

--- botEvents/interactionCreate.js
+++ botEvents/interactionCreate.js
@@ -25,8 +25,25 @@ module.exports = {
 
             // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT
             if(interaction.customId === 'begin_verification_button') {
-                interaction.user.send(`${clickUser}, you have pressed the "Begin Verification" button.`)
-                console.log(`The "Begin Verification" button has been clicked by ${clickUser}.`) 
+
+                // CHECK USER PERMISSIONS FOR THE VERIFIED ROLE
+                    // IF VERIFIED, CANCEL AND RESPOND
+
+
+                // EMPHEMERAL REPLY TO BUTTON PRESS TO LET USER KNOW TO CHECK THEIR DMS
+                interaction.reply({ content: 'Please check for a DM from HooterBot to complete your verification.', ephemeral: true })
+
+
+                // CREATE TICKET CHANNEL
+
+                // LOG DATABASE INFORMATION FOR TICKET
+
+                // DMING USER TEST
+                interaction.user.send(`${clickUser}, you have pressed the "Begin Verification" button.\nclickUserTag = ${clickUserTag}\nclickUsername = ${clickUsername}\nclickUserId = ${clickUserId}\nClick date = ${Date.now()}`)
+                console.log(`The "Begin Verification" button has been clicked by ${clickUser}.`)
+
+
+
             }
 
             // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT SET
