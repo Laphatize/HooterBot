0a8522addd15de08baf1d1afd14d5468d34312b3
Liam Chambers <leemleem1234@outlook.com>
2021-07-10 14:54:55 -0400

    Testing interaction fix

--- botEvents/interactionCreate.js
+++ botEvents/interactionCreate.js
@@ -9,27 +9,38 @@ module.exports = {
 	async execute(interaction) {
 
         // IGNORNING NON-BUTTON INTERACTIONS
-        if(!interaction.isButton()) return;
+        if(interaction.isButton()) {
 
-        // GETTING PERSON WHO CLICKED BUTTON (OBJECT)
-        let clickUser = await interaction.user.fetch();   // user = @MrMusicMan789 (OBJECT)
-        let clickUserTag = clickUser.tag;                    // clickUserTag = MrMusicMan789#0789
-        let clickUsername = clickUser.username;              // clickUsername = MrMusicMan789
-        let clickUserId = clickUser.id;                      // clickUserID = 472185023622152203
+            // GETTING PERSON WHO CLICKED BUTTON (OBJECT)
+            let clickUser = await interaction.user.fetch();   // user = @MrMusicMan789 (OBJECT)
+            let clickUserTag = clickUser.tag;                    // clickUserTag = MrMusicMan789#0789
+            let clickUsername = clickUser.username;              // clickUsername = MrMusicMan789
+            let clickUserId = clickUser.id;                      // clickUserID = 472185023622152203
 
-        console.log(`clickUser = ${clickUser}`);
-        console.log(`clickUserTag = ${clickUserTag}`);
-        console.log(`clickUsername = ${clickUsername}`);
-        console.log(`clickUserId = ${clickUserId}`);
+            console.log(`clickUser = ${clickUser}`);
+            console.log(`clickUserTag = ${clickUserTag}`);
+            console.log(`clickUsername = ${clickUsername}`);
+            console.log(`clickUserId = ${clickUserId}`);
 
 
-        // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT
-        if(interaction.customId === 'begin_verification_button') {
-            interaction.reply(`${clickUser}, you have pressed the "Begin Verification" button.`)
-            console.log(`The "Begin Verification" button has been clicked by ${clickUser}.`) 
+            // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT
+            if(interaction.customId === 'begin_verification_button') {
+                interaction.reply(`${clickUser}, you have pressed the "Begin Verification" button.`)
+                console.log(`The "Begin Verification" button has been clicked by ${clickUser}.`) 
+            }
+
+            // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT SET
+
+
+
+
+
+
+
+
+
+
+            
         }
-
-        // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT SET
-
 	},
 };
