49d785288f19433a29ae587b5f3a14f0e8ca0398
Liam Chambers <leemleem1234@outlook.com>
2021-07-10 13:22:17 -0400

    Logging interaction, date fix

--- botEvents/interactionCreate.js
+++ botEvents/interactionCreate.js
@@ -11,22 +11,26 @@ module.exports = {
         // IGNORNING NON-BUTTON INTERACTIONS
         if(!interaction.isButton()) return;
 
-        // GETTING PERSON WHO CLICKED BUTTON (OBJECT)
-        let clickUser = await interaction.user.fetch();   // user = @MrMusicMan789 (OBJECT)
-        let clickUserTag = clickUser.tag;                    // clickUserTag = MrMusicMan789#0789
-        let clickUsername = clickUser.username;              // clickUsername = MrMusicMan789
-        let clickUserId = clickUser.id;                      // clickUserID = 472185023622152203
+
+        console.log(interaction);
+        return;
+
+        // // GETTING PERSON WHO CLICKED BUTTON (OBJECT)
+        // let clickUser = await interaction.user.fetch();   // user = @MrMusicMan789 (OBJECT)
+        // let clickUserTag = clickUser.tag;                    // clickUserTag = MrMusicMan789#0789
+        // let clickUsername = clickUser.username;              // clickUsername = MrMusicMan789
+        // let clickUserId = clickUser.id;                      // clickUserID = 472185023622152203
 
 
-        // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT SET
-        const filter = i => i.customId === 'begin_verification_button'
-        interaction.channel.awaitMessageComponent({ filter })
-            .then(i => {
-                interaction.channel.send(`${clickUser}, you have pressed the "Begin Verification" button.`)
-                console.log(`The "Begin Verification" button has been clicked by ${clickUser}.`) 
-            })
+        // // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT
+        // const filter = i => i.customId === 'begin_verification_button'
+        // interaction.channel.awaitMessageComponent({ filter })
+        //     .then(i => { console.log(i)
+        //         interaction.channel.send(`${clickUser}, you have pressed the "Begin Verification" button.`)
+        //         console.log(`The "Begin Verification" button has been clicked by ${clickUser}.`) 
+        //     })
 
-        // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT SET
+        // // INITIAL VERIFICATION PROMPT - SET VIA FILTER FOR CUSTOMID - NO TIMEOUT SET
 
 	},
 };
