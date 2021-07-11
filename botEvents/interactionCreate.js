62eb448e4e63ed10029b399767ec87b621276e42
Liam Chambers <leemleem1234@outlook.com>
2021-07-10 21:05:22 -0400

    Commenting out for logging.

--- botEvents/interactionCreate.js
+++ botEvents/interactionCreate.js
@@ -17,119 +17,118 @@ module.exports = {
             /***********************************************************/
             if(interaction.customId === 'begin_verification_button') {
 
+                console.log(`interaction.member =${interaction.member}`)
+
                 // GRAB VERIFIED ROLE FROM GUILD
                 const verifiedRole = await interaction.guild.roles.cache.find((role) => role.id === config.verifiedRoleID)
 
-                // FETCHING GUILD MEMBER WHO PRESSED BUTTON
-                buttonPresser = interaction.member.fetch(interaction.member.id);
+                console.log(`\nverifiedRole = ${verifiedRole}\n`)
 
-                console.log(`buttonPresser = ${buttonPresser}`)
+            //     // CHECK IF USER HAS VERIFIED ROLE
+            //     if(buttonPresser.has(verifiedRole)) {
 
-                // CHECK IF USER HAS VERIFIED ROLE
-                if(buttonPresser.roles.cache.has(verifiedRole)) {
+            //         console.log(`${interaction.member.fetch().username} has started verification but already possesses the verified role!`)
 
-                    console.log(`${interaction.member.fetch().username} has started verification but already possesses the verified role!`)
+            //         // CANCEL AND RESPOND WITH EPHEMERAL
+            //         return interaction.reply({ content: `Sorry, you're already verified!\n(If this is an error, please submit a ModMail ticket and let us know.)`, ephemeral: true })
+            //     }
 
-                    // CANCEL AND RESPOND WITH EPHEMERAL
-                    return interaction.reply({ content: `Sorry, you're already verified!\n(If this is an error, please submit a ModMail ticket and let us know.)`, ephemeral: true })
-                }
-
-                // EMPHEMERAL REPLY TO BUTTON PRESS - LET USER KNOW TO CHECK THEIR DMS
-                interaction.reply({ content: `**Verification started!** Please check for a DM from HooterBot to complete your verification.\n***Didn't receive a DM?*** Make sure you allow DMs from server members in your privacy settings.`, ephemeral: true })
-                    .catch(err => console.log(err));
+            //     // EMPHEMERAL REPLY TO BUTTON PRESS - LET USER KNOW TO CHECK THEIR DMS
+            //     interaction.reply({ content: `**Verification started!** Please check for a DM from HooterBot to complete your verification.\n***Didn't receive a DM?*** Make sure you allow DMs from server members in your privacy settings.`, ephemeral: true })
+            //         .catch(err => console.log(err));
 
 
-                // CREATE TICKET CHANNEL
+            //     // CREATE TICKET CHANNEL
 
-                    // *****NEED TO ADD*****
-
-
-                // LOG DATABASE INFORMATION FOR TICKET
-
-                    // *****NEED TO ADD*****
+            //         // *****NEED TO ADD*****
 
 
-                // GENERATING INITIAL EMBED FOR DM
-                let ticketEmbed = new discord.MessageEmbed()
-                    .setColor(config.embedTempleRed)
-                    .setTitle(`**Verification - Ticket Opened**`)
-                    .setDescription(`Thanks for wanting to verify in the <:TempleT:857293539779018773> **Temple University server**.
-                        \nThere are three ways you can verify you are a student or employee:
-                        \n${config.indent}**1.** Use a physical TUid card
-                        \n${config.indent}**2.** Use a virtual TUid card
-                        \n${config.indent}**3.** Using TUportal
-                        \n\nSelect the method using the buttons below to receive instructions. You can quit verification at any time using the red "Quit Verification" button.\n`)
+            //     // LOG DATABASE INFORMATION FOR TICKET
 
-                // INITIALIZING BUTTON
-                let TUidCardButton = new MessageButton()
-                    .setLabel("Physical TUid Card")
-                    .setStyle("SECONDARY")
-                    .setCustomId("physical_TUid_Card")
-                    .setDisabled(true)
-                let VirtualTUidCardButton = new MessageButton()
-                    .setLabel("Virtual TUid Card")
-                    .setStyle("SECONDARY")
-                    .setCustomId("virtual_TUid_Card")
-                    .setDisabled(true)
-                let TuPortalButton = new MessageButton()
-                    .setLabel("TUportal")
-                    .setStyle("SECONDARY")
-                    .setCustomId("TU_portal")
-                    .setDisabled(true)
-                let CancelButton = new MessageButton()
-                    .setLabel("Quit Verification")
-                    .setStyle("DANGER")
-                    .setCustomId("quit")
-                    .setDisabled(false)
-
-                // BUTTON ROW
-                let buttonRow = new MessageActionRow()
-                .addComponents(
-                    TUidCardButton,
-                    VirtualTUidCardButton,
-                    TuPortalButton,
-                    CancelButton
-                );
-
-                // DMING USER THE INITIAL VERIFICATION PROMPT
-                interaction.user.send({embeds: [ticketEmbed], components: [buttonRow] })
-                    .catch(err => console.log(err))
-            }
-            // END OF "BEGIN VERIFICATION" PROMPT BUTTON
+            //         // *****NEED TO ADD*****
 
 
-            /***********************************************************/
-            /*      QUIT VERIFICATION (ANY PROMPT IN DMS)              */
-            /***********************************************************/
-            if(interaction.customId === 'quit') {
+            //     // GENERATING INITIAL EMBED FOR DM
+            //     let ticketEmbed = new discord.MessageEmbed()
+            //         .setColor(config.embedTempleRed)
+            //         .setTitle(`**Verification - Ticket Opened**`)
+            //         .setDescription(`Thanks for wanting to verify in the <:TempleT:857293539779018773> **Temple University server**.
+            //             \nThere are three ways you can verify you are a student or employee:
+            //             \n${config.indent}**1.** Use a physical TUid card
+            //             \n${config.indent}**2.** Use a virtual TUid card
+            //             \n${config.indent}**3.** Using TUportal
+            //             \n\nSelect the method using the buttons below to receive instructions. You can quit verification at any time using the red "Quit Verification" button.\n`)
 
-                // GENERATING QUIT CONFIRMATION EMBED FOR DM
-                let quitConfirmEmbed = new discord.MessageEmbed()
-                    .setColor(config.embedTempleRed)
-                    .setTitle(`**Close confirmation.**`)
-                    .setDescription(`Please confirm ticket cancellation.`)
+            //     // INITIALIZING BUTTON
+            //     let TUidCardButton = new MessageButton()
+            //         .setLabel("Physical TUid Card")
+            //         .setStyle("SECONDARY")
+            //         .setCustomId("physical_TUid_Card")
+            //         .setDisabled(true)
+            //     let VirtualTUidCardButton = new MessageButton()
+            //         .setLabel("Virtual TUid Card")
+            //         .setStyle("SECONDARY")
+            //         .setCustomId("virtual_TUid_Card")
+            //         .setDisabled(true)
+            //     let TuPortalButton = new MessageButton()
+            //         .setLabel("TUportal")
+            //         .setStyle("SECONDARY")
+            //         .setCustomId("TU_portal")
+            //         .setDisabled(true)
+            //     let CancelButton = new MessageButton()
+            //         .setLabel("Quit Verification")
+            //         .setStyle("DANGER")
+            //         .setCustomId("quit")
+            //         .setDisabled(false)
 
-                // INITIALIZING BUTTON
-                let quitConfirmButton = new MessageButton()
-                    .setLabel("Yes, Quit")
-                    .setStyle("DANGER")
-                    .setCustomId("quit_confirmation")
-                let cancelQuitButton = new MessageButton()
-                    .setLabel("Cancel")
-                    .setStyle("SECONDARY")
-                    .setCustomId("cancel_quit")
+            //     // BUTTON ROW
+            //     let buttonRow = new MessageActionRow()
+            //     .addComponents(
+            //         TUidCardButton,
+            //         VirtualTUidCardButton,
+            //         TuPortalButton,
+            //         CancelButton
+            //     );
+
+            //     // DMING USER THE INITIAL VERIFICATION PROMPT
+            //     interaction.user.send({embeds: [ticketEmbed], components: [buttonRow] })
+            //         .catch(err => console.log(err))
+            // }
+            // // END OF "BEGIN VERIFICATION" PROMPT BUTTON
+
+
+            // /***********************************************************/
+            // /*      QUIT VERIFICATION (ANY PROMPT IN DMS)              */
+            // /***********************************************************/
+            // if(interaction.customId === 'quit') {
+
+            //     // GENERATING QUIT CONFIRMATION EMBED FOR DM
+            //     let quitConfirmEmbed = new discord.MessageEmbed()
+            //         .setColor(config.embedTempleRed)
+            //         .setTitle(`**Close confirmation.**`)
+            //         .setDescription(`Please confirm ticket cancellation.`)
+
+            //     // INITIALIZING BUTTON
+            //     let quitConfirmButton = new MessageButton()
+            //         .setLabel("Yes, Quit")
+            //         .setStyle("DANGER")
+            //         .setCustomId("quit_confirmation")
+            //     let cancelQuitButton = new MessageButton()
+            //         .setLabel("Cancel")
+            //         .setStyle("SECONDARY")
+            //         .setCustomId("cancel_quit")
         
-                // BUTTON ROW
-                let buttonRow = new MessageActionRow()
-                .addComponents(
-                    quitConfirmButton
-                );
+            //     // BUTTON ROW
+            //     let buttonRow = new MessageActionRow()
+            //     .addComponents(
+            //         quitConfirmButton
+            //     );
 
-                // DMING USER THE INITIAL QUIT PROMPT
-                interaction.user.send({embeds: [quitConfirmEmbed], components: [buttonRow, cancelQuitButton] })
-                    .catch(err => console.log(err))
-            }
-            // END OF "QUIT" BUTTON
+            //     // DMING USER THE INITIAL QUIT PROMPT
+            //     interaction.user.send({embeds: [quitConfirmEmbed], components: [buttonRow, cancelQuitButton] })
+            //         .catch(err => console.log(err))
+            // }
+            // // END OF "QUIT" BUTTON
 
 
             /***********************************************************/
