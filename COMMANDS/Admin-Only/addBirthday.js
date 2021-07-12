96eab3902d20629d051be627656eadce841bfd68
Liam Chambers <leemleem1234@outlook.com>
2021-07-12 16:03:38 -0400

    fixing bdayUser guild member search

--- COMMANDS/Admin-Only/addBirthday.js
+++ COMMANDS/Admin-Only/addBirthday.js
@@ -39,7 +39,7 @@ module.exports = {
         // CHECK THAT THE USER EXISTS IN THE GUILD
         let guild = client.guilds.cache.get(message.guild.id);
         
-        if(guild.member(bdayUserId)) {
+        if(guild.members.cache.find(bdayUserId)) {
             let bdayUseDNEEmbed = new discord.MessageEmbed()
                 .setColor(config.embedTempleRed)
                 .setTitle(`${config.emjREDTICK} **Error!**`)
