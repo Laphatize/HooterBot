7cc79ce2e219807e0f7f8d3fbfca03d679d2babc
Liam Chambers <leemleem1234@outlook.com>
2021-07-13 17:12:59 -0400

    Logging check

--- botEvents/interactionCreate.js
+++ botEvents/interactionCreate.js
@@ -172,6 +172,9 @@ module.exports = {
                     .addField(`User ID:`, `${interaction.user.id}`, true)
                     .setFooter(`Please do not send a message in this channel unless it is in response to a user's question.`)
 
+                    console.log(`firstDMmsg = ${firstDMmsg}`)
+
+                    return;
 
                     // FETCHING THE NEW TICKET TEXT CHANNEL TO SEND TICKET START EMBED
                     newTicketChannel = interaction.guild.channels.cache.find(ch => ch.name.toLowerCase() === ticketChannelName.toLowerCase())
