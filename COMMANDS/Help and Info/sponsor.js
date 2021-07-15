1b03247511dfa8a03b0ad09f90348f24d013734a
Liam Chambers <leemleem1234@outlook.com>
2021-07-15 02:08:29 -0400

    Sponsor tweaks.

--- COMMANDS/Help and Info/sponsor.js
+++ COMMANDS/Help and Info/sponsor.js
@@ -23,11 +23,10 @@ module.exports = {
         let infoEmbed = new discord.MessageEmbed()
         .setColor(config.embedBlurple)
         .setTitle(`**Support HooterBot!**`)
-        .setDescription(`${config.botName} is built by <@${config.botAuthorId}> in his free time to improve the server. HooterBot runs on the newest version of Discord.js (✨ v13, *fancy...*✨), uses brand new features from Discord like buttons, interactions, slash commands (coming Soon™), ephemeral messages, and more! The code is open soruce (see link below).
+        .setDescription(`${config.botName} is built by <@${config.botAuthorId}> in his free time to improve the server. HooterBot runs on the newest version of Discord.js (✨ ***v13***, *fancy...* ✨), uses brand new features from Discord like buttons, interactions, slash commands (coming Soon™), ephemeral messages, and more! The code is open soruce (see link below).
         \nThis is not ${config.botAuthorUsername}'s first Discord bot, but it is his first to utilize and rely on a database connection for functionality. As ${config.botName} grows with new features, the costs to keep the bot online will increase.
-        \nIf you would like to support the development and operation of HooterBot, check out ${config.botAuthorUsername}'s GitHub Sponsor page at the link below.`)
+        \nIf you want to support ${config.botName}'s development and operations, check out ${config.botAuthorUsername}'s GitHub Sponsor page:`)
         .addField(`GitHub Sponsor Page`, `https://github.com/sponsors/MrMusicMan789`)
-        .addField(`GitHub Code Repository`, `${pjson.repository.url.split(`+`).pop()}`)
         .setThumbnail('https://avatars.githubusercontent.com/u/58273574?v=4')
 
         
