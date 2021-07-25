53a390ec8f65160f5e716c23c071e1d9f61953d8
Liam Chambers <leemleem1234@outlook.com>
2021-07-25 16:15:41 -0400

    Update messageCreate.js

--- botEvents/messageCreate.js
+++ botEvents/messageCreate.js
@@ -71,6 +71,9 @@ module.exports = {
                     .catch(err => {
                         message.channel.send(`This ticket is closed. Messages can not be sent to the ticket channel any more.`)
                     })
+
+                    
+                    // ADD SUCCESS EMOJI TO THE ORIGINAL DM MESSAGE ONCE SENT
             }
         }
 
@@ -91,31 +94,24 @@ module.exports = {
 
 
             // FETCH THE USER FROM THE CHANNEL NAME
-            message.guild.members.fetch({ query: dmUsername, limit: 1 })
-                .then(user => {
-
-                    // GRABBING MESSAGE CONTENT AND FORMATTING FOR EMBED
-                    let userTicketMsg = new discord.MessageEmbed()
-                        .setColor(config.embedGrey)
-                        .setAuthor(message.author.username, message.author.displayAvatarURL())
-                        .setDescription(message.content)
-                        .setTimestamp()
+            const ticketUser = await message.guild.members.fetch({ query: dmUsername, limit: 1 })
 
 
-                    // SENDING MESSAGE FROM MOD/ADMIN TICKET CHANNEL TO USER IN DMs
-                    return user.send({ embeds: [userTicketMsg] })
-                        .catch(err => {
-                            message.channel.send(`This ticket is closed. Messages can not be sent to the user any more.`)
-                        })
+            // GRABBING MESSAGE CONTENT AND FORMATTING FOR EMBED
+            let userTicketMsg = new discord.MessageEmbed()
+                .setColor(config.embedGrey)
+                .setAuthor(message.author.username, message.author.displayAvatarURL())
+                .setDescription(message.content)
+                .setTimestamp()
+
+
+            // SENDING MESSAGE FROM MOD/ADMIN TICKET CHANNEL TO USER IN DMs
+            return ticketUser.send({ embeds: [userTicketMsg] })
+                .catch(err => {
+                    message.channel.send(`${config.emjREDTICK} This ticket has been closed. Messages can not be sent to the user.`)
                 })
+                // .then ADD SUCCESS EMOJI TO THE ORIGINAL TICKET MESSAGE ONCE SENT
         }
-
-
-
-
-
-
-
 
 
 
