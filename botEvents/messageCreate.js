cc02eaabeeaf1457e69749cbbc584ddb3db01e42
Liam Chambers <leemleem1234@outlook.com>
2021-07-25 16:30:25 -0400

    Trying fix for DM user grab

--- botEvents/messageCreate.js
+++ botEvents/messageCreate.js
@@ -92,9 +92,7 @@ module.exports = {
 
             // FETCH THE USER FROM THE CHANNEL NAME
             // THE USER HAS TO BE IN THIS GUILD SINCE THE TICKET IS IN THE GUILD
-            const ticketUser = await message.guild.members.fetch({ query: dmUsername, limit: 1 })
-
-            console.log(`ticketUser = ${ticketUser}`)
+            const ticketUser = await message.guild.users.cache.find(user => user.username == dmUsername)
 
 
             // GRABBING MESSAGE CONTENT AND FORMATTING FOR EMBED
