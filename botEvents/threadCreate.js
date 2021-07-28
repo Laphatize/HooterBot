789ef404b99ca80623b13207de7881e7874dc4f9
Liam Chambers <leemleem1234@outlook.com>
2021-07-28 16:32:50 -0400

    Testing logging for thread creation

--- botEvents/threadCreate.js
+++ botEvents/threadCreate.js
@@ -7,18 +7,19 @@ module.exports = {
 
         console.log('A thread was created.')
 
-        // // LOG ENTRY
-        // // GENERATE NOTICE EMBED
-        // let threadLogEntry = new discord.MessageEmbed()
-        //     .setColor(config.embedGrey)
-        //     .setTitle(`Thread Created`)
-        //     .addField(`Creator:`, `${threadmember}`)
-        //     .addField(`User ID:`, `${threadmember.id}`, true)
-        //     .addField(`Thread:`, `${channel}`)
-        //     .setTimestamp()
+        // LOG ENTRY
+        // GENERATE NOTICE EMBED
+        let threadLogEntry = new discord.MessageEmbed()
+            .setColor(config.embedGrey)
+            .setTitle(`Thread Created`)
+            .addField(`threadmember:`, `${threadmember}`, true)
+            .addField(`threadmember.id:`, `${threadmember.id}`, true)
+            .addField(`channel`, `${channel}`, true)
+            .addField(`parent_id`, `${parent_id}`, true)
+            .setTimestamp()
 
-        // // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
-        // client.channels.cache.get(config.logActionsChannelId).send({ embeds: [threadLogEntry] })
-        //     .catch(err => console.log(err))
+        // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
+        client.channels.cache.get(config.logActionsChannelId).send({ embeds: [threadLogEntry] })
+            .catch(err => console.log(err))
 	},
 };
--- botEvents/threadDelete.js
+++ botEvents/threadDelete.js
@@ -3,22 +3,24 @@ const config = require('../config.json');
 
 module.exports = {
 	name: 'threadDelete',
-	async execute(channel, threadmember, client) {
+	async execute(channel, client) {
 
         console.log('A thread was deleted.')
 
-        // // LOG ENTRY
-        // // GENERATE NOTICE EMBED
-        // let threadLogEntry = new discord.MessageEmbed()
-        //     .setColor(config.embedGrey)
-        //     .setTitle(`Thread Created`)
-        //     .addField(`Creator:`, `${threadmember}`)
-        //     .addField(`User ID:`, `${threadmember.id}`, true)
-        //     .addField(`Thread:`, `${channel}`)
-        //     .setTimestamp()
 
-        // // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
-        // client.channels.cache.get(config.logActionsChannelId).send({ embeds: [threadLogEntry] })
-        //     .catch(err => console.log(err))
+        parentChannel = client.channels.cache.get(parent_id)
+
+        // LOG ENTRY
+        // GENERATE NOTICE EMBED
+        let threadLogEntry = new discord.MessageEmbed()
+            .setColor(config.embedGrey)
+            .setTitle(`Thread Deleted`)
+            .addField(`Parent Channel:`, `${parentChannel}`, true)
+            .addField(`Thread:`, `${channel.name}`)
+            .setTimestamp()
+
+        // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
+        client.channels.cache.get(config.logActionsChannelId).send({ embeds: [threadLogEntry] })
+            .catch(err => console.log(err))
 	},
 };
