5b0cef6414542b1ab1840f32e3751695ef9ebeed
Liam Chambers <leemleem1234@outlook.com>
2021-07-28 16:42:35 -0400

    parent id fix

--- botEvents/threadCreate.js
+++ botEvents/threadCreate.js
@@ -7,7 +7,7 @@ module.exports = {
 
         console.log('A thread was created.')
 
-        parentChannel = client.channels.cache.get(channel.parent_id)
+        parentChannel = client.guild.channels.cache.get(channel.parent_id)
 
         // LOG ENTRY
         // GENERATE NOTICE EMBED
