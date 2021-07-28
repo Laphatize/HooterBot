71f7c1b7ab076b3a658b2f1797b6ee2e1e7b8819
Liam Chambers <leemleem1234@outlook.com>
2021-07-28 16:15:56 -0400

    Testing thread events

---
+++ botEvents/threadUpdate.js
@@ -0,0 +1,24 @@
+const discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports = {
+	name: 'threadUpdate',
+	async execute(channel, threadmember, client) {
+
+        console.log('A thread was updated.')
+
+        // // LOG ENTRY
+        // // GENERATE NOTICE EMBED
+        // let threadLogEntry = new discord.MessageEmbed()
+        //     .setColor(config.embedGrey)
+        //     .setTitle(`Thread Created`)
+        //     .addField(`Creator:`, `${threadmember}`)
+        //     .addField(`User ID:`, `${threadmember.id}`, true)
+        //     .addField(`Thread:`, `${channel}`)
+        //     .setTimestamp()
+
+        // // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
+        // client.channels.cache.get(config.logActionsChannelId).send({ embeds: [threadLogEntry] })
+        //     .catch(err => console.log(err))
+	},
+};
