9713e75a5b14521348fd893b719e76a660632ac8
Liam Chambers <leemleem1234@outlook.com>
2021-07-29 15:53:06 -0400

    Changing birthday role application

--- bot.js
+++ bot.js
@@ -109,7 +109,7 @@ process.on('unhandledRejection', err => {
 // SCHEDULER FORMAT: *(Second) *(Minute) *(Hour) *(Day of Month) *(Month) *(Day of Week)
 
 // BIRTHDAY CHECKS - EVERY DAY AT 7:00AM EST
-cron.schedule('00 52 15 * * *', async () => {
+cron.schedule('00 55 15 * * *', async () => {
     
     console.log('Checking for birthdays...');
 
@@ -157,13 +157,12 @@ cron.schedule('00 52 15 * * *', async () => {
 
             // FETCH BIRTHDAY USER BY ID, GIVE ROLE
             bdayUser = guild.members.fetch(id)
-
-            console.log(`bdayUser fetched:\n${bdayUser}\n\nNow giving role...`)
-
-            bdayRole = guild.roles.cache.find(role => role.id === 870118399383064607)
+            .then(user => {
+                bdayRole = guild.roles.cache.find(role => role.id === 870118399383064607)
             
-            bdayUser.roles.add(bdayRole)
-            console.log(`The birthday role (ID: ${bdayRole.id}) should have been added to the user.`)
+                user.roles.add(bdayRole)
+                console.log(`The birthday role should have been added to the user.`)
+            })
         })
     }
 }, {
