3a4be612d8ca7091f6acad144f226e450efb2f96
Liam Chambers <leemleem1234@outlook.com>
2021-07-29 02:22:06 -0400

    Logging

--- botEvents/threadUpdate.js
+++ botEvents/threadUpdate.js
@@ -10,22 +10,32 @@ module.exports = {
         console.log(`channel.guild.id = ${channel.guild.id}`) // GUILD ID
 
 
-        // FETCHING GUILD AND THEN ITS MOD-LOG CHANNEL
-        const modLogChannel = client.guilds.cache.get(channel.guild.id).channels.cache.find(ch => ch.name === `mod-log`)
+        // FETCHING GUILD BY NAME.
+        let guild = client.guilds.cache.find(ch => ch.name === channel.guild)
+
+        console.log(`guild = ${guild}`)
 
         
-        // LOG ENTRY
-        // GENERATE NOTICE EMBED
-        let threadLogEntry = new discord.MessageEmbed()
-            .setColor(config.embedGrey)
-            .setTitle(`Thread Updated`)
-            .addField(`Thread:`, `${channel}`, true)
-            .addField(`Parent Channel:`, `<#${channel.parent.id}>`, true)
-            .setDescription(`This could mean the thread has been renamed or the thread has been archived. The API makes no distinction.`)
-            .setTimestamp()
+        // // FETCHING GUILD AND THEN ITS MOD-LOG CHANNEL
+        // const modLogChannel = client.guilds.cache.get(channel.guild.id)
+        
+        
+        
+        // .channels.cache.find(ch => ch.name === `mod-log`)
 
-        // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
-        modLogChannel.send({ embeds: [threadLogEntry] })
-            .catch(err => console.log(err))
+        
+        // // LOG ENTRY
+        // // GENERATE NOTICE EMBED
+        // let threadLogEntry = new discord.MessageEmbed()
+        //     .setColor(config.embedGrey)
+        //     .setTitle(`Thread Updated`)
+        //     .addField(`Thread:`, `${channel}`, true)
+        //     .addField(`Parent Channel:`, `<#${channel.parent.id}>`, true)
+        //     .setDescription(`This could mean the thread has been renamed or the thread has been archived. The API makes no distinction.`)
+        //     .setTimestamp()
+
+        // // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
+        // modLogChannel.send({ embeds: [threadLogEntry] })
+        //     .catch(err => console.log(err))
 	},
 };
