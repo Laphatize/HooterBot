0c1c6520be78b0d918a9a3b36124af70c2d8c1a9
Liam Chambers <leemleem1234@outlook.com>
2021-07-29 02:05:43 -0400

    logging

--- botEvents/threadUpdate.js
+++ botEvents/threadUpdate.js
@@ -5,20 +5,22 @@ module.exports = {
 	name: 'threadUpdate',
 	async execute(channel, client) {
 
-        const modLogChannel = channel.guild.cache.get(guild.id).channels.cache.find(ch => ch.name === `mod-log`)
+        console.log(`channel.guild = ${channel.guild}`)
 
-        // LOG ENTRY
-        // GENERATE NOTICE EMBED
-        let threadLogEntry = new discord.MessageEmbed()
-            .setColor(config.embedGrey)
-            .setTitle(`Thread Updated`)
-            .addField(`Thread:`, `${channel}`, true)
-            .addField(`Parent Channel:`, `<#${channel.parent.id}>`, true)
-            .setDescription(`This could mean the thread has been renamed or the thread has been archived. The API makes no distinction.`)
-            .setTimestamp()
+        // const modLogChannel = channel.guild.cache.get(guild.id).channels.cache.find(ch => ch.name === `mod-log`)
 
-        // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
-        modLogChannel.send({ embeds: [threadLogEntry] })
-            .catch(err => console.log(err))
+        // // LOG ENTRY
+        // // GENERATE NOTICE EMBED
+        // let threadLogEntry = new discord.MessageEmbed()
+        //     .setColor(config.embedGrey)
+        //     .setTitle(`Thread Updated`)
+        //     .addField(`Thread:`, `${channel}`, true)
+        //     .addField(`Parent Channel:`, `<#${channel.parent.id}>`, true)
+        //     .setDescription(`This could mean the thread has been renamed or the thread has been archived. The API makes no distinction.`)
+        //     .setTimestamp()
+
+        // // FETCHING LOG CHANNEL AND SENDING CLOSURE NOTICE
+        // modLogChannel.send({ embeds: [threadLogEntry] })
+        //     .catch(err => console.log(err))
 	},
 };
