c324894a35b541a7bd9818694980f7e3e090cdb1
Liam Chambers <leemleem1234@outlook.com>
2021-07-30 16:39:37 -0400

    Creating admin and mod help

---
+++ COMMANDS/Help and Info/adminhelp.js
@@ -0,0 +1,44 @@
+const discord = require('discord.js')
+const { MessageActionRow, MessageButton } = require('discord.js');
+const fs = require(`fs`)
+const config = require ('../../config.json')
+
+module.exports = {
+    name: `adminhelp`,
+    aliases: [`admincommands`],
+    description: `Describes ${config.botName}'s commands for administrators. (🗺️📌 *You are here*)`,
+    category: `Help and Info`,
+    expectedArgs: '',
+    cooldown: 60,
+    minArgs: 0,
+    maxArgs: 0,
+    permissions: 'ADMINISTRATOR',
+    requiredRoles: [],
+    execute: async (message, arguments, client) => {
+
+        let helpEmbed = new discord.MessageEmbed()
+            .setColor(config.embedBlurple)
+            .setTitle(`**Admin Help:**`)
+            .setDescription(`Select a button below for information on commands in that category.`)
+
+        let AdminOnlyButton = new MessageButton()
+            .setLabel("Admin")
+            .setStyle(`SECONDARY`)
+            .setCustomId("help_admin")
+        let VerificationButton = new MessageButton()
+            .setLabel("Verification")
+            .setStyle(`SECONDARY`)
+            .setCustomId("help_verification")
+        
+
+        // BUTTON ROWS BASED ON ADMIN OR NOT
+        
+        let mainHelpRow = new MessageActionRow()
+            .addComponents(
+                AdminOnlyButton,
+                VerificationButton,
+            );
+
+        return message.reply({ embeds: [helpEmbed], components: [mainHelpRow] })
+    }
+}
---
+++ COMMANDS/Help and Info/modhelp.js
@@ -0,0 +1,39 @@
+const discord = require('discord.js')
+const { MessageActionRow, MessageButton } = require('discord.js');
+const fs = require(`fs`)
+const config = require ('../../config.json')
+
+module.exports = {
+    name: `modhelp`,
+    aliases: [`modcommands`],
+    description: `Describes ${config.botName}'s commands for moderators. (🗺️📌 *You are here*)`,
+    category: `Help and Info`,
+    expectedArgs: '',
+    cooldown: 60,
+    minArgs: 0,
+    maxArgs: 0,
+    permissions: 'ADMINISTRATOR',
+    requiredRoles: [],
+    execute: async (message, arguments, client) => {
+
+        let helpEmbed = new discord.MessageEmbed()
+            .setColor(config.embedBlurple)
+            .setTitle(`**Admin Help:**`)
+            .setDescription(`Select a button below for information on commands in that category.`)
+
+        let ModOnlyButton = new MessageButton()
+            .setLabel("Moderator")
+            .setStyle(`SECONDARY`)
+            .setCustomId("help_admin")
+        
+
+        // BUTTON ROWS BASED ON ADMIN OR NOT
+        
+        let mainHelpRow = new MessageActionRow()
+            .addComponents(
+                ModOnlyButton,
+            );
+
+        return message.reply({ embeds: [helpEmbed], components: [mainHelpRow] })
+    }
+}
