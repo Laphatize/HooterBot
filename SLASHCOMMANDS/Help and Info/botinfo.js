84ff537a1d0c97d64c9bc9387b6b72eedc8072e3
Liam Chambers <leemleem1234@outlook.com>
2021-07-31 01:14:25 -0400

    Botinfo slash command

--- SLASHCOMMANDS/Help and Info/botinfo.js
+++ SLASHCOMMANDS/Help and Info/botinfo.js
@@ -1,24 +1,14 @@
 const discord = require('discord.js')
+const { CommandInteraction } = require('discord.js')
 var pjson = require('../../package.json');
 const config = require ('../../config.json')
 
 module.exports = {
-    name: `botinfo`,
-    aliases: [`info`],
+    name: 'botinfo',
     description: `Describes details about ${config.botName}.`,
-    category: `Help and Info`,
-    expectedArgs: '',
-    cooldown: 60,
-    minArgs: 0,
-    maxArgs: 0,
-    permissions: '',
-    requiredRoles: [],
-    execute: (message, arguments, client) => {
+    run: async(client, interaction, args) => {
 
-        // DELETING INVOCATION MESSAGE
-        setTimeout(() => message.delete(), 0 );
-
-
+        
         // UPTIME CALCULATION
         let totalSeconds = (client.uptime / 1000);
         let days = Math.floor(totalSeconds / 86400);
@@ -28,14 +18,16 @@ module.exports = {
         let minutes = Math.floor(totalSeconds / 60);
         let seconds = Math.floor(totalSeconds % 60);
 
+
         let botUptime = `${days}D, ${hours}H, ${minutes}M and ${seconds}S`;
-        
         let DJSv = pjson.dependencies['discord.js'].split(`^`).pop().split('-', 1)[0]
 
+        // IF DEV BRANCH OF DJS
         if(pjson.dependencies['discord.js'].split(`^`).pop().includes('dev')) {
             devVer = pjson.dependencies['discord.js'].split(`-`).pop()
             DJSversion = DJSv.concat(`\n*(${devVer})*`)
         }
+        // OTHERWISE, JUST DJS
         else {
             DJSversion = DJSv;
         }
@@ -55,8 +47,6 @@ module.exports = {
             .addField(`GitHub Repository`, `${pjson.repository.url.split(`+`).pop()}`)
             .setThumbnail('https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/HooterBot_Square_Shadow.png')
 
-        
-        // RESPONDING TO USER WITH INFO EMBED
-        message.channel.send({embeds: [infoEmbed]})
+        interaction.reply({ embeds: [infoEmbed] })
     }
 }
