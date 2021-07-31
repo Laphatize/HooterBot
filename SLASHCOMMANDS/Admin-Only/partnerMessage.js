6bc64caa87f652183e5140a2f745c2b1f5d7bd5a
Liam Chambers <leemleem1234@outlook.com>
2021-07-31 02:03:21 -0400

    Testing args acquisition

--- SLASHCOMMANDS/Admin-Only/partnerMessage.js
+++ SLASHCOMMANDS/Admin-Only/partnerMessage.js
@@ -4,27 +4,30 @@ const config = require ('../../config.json')
 module.exports = {
     name: 'partnermessage',
     description: `(ADMIN) Generate an embed in \#server-announcements to promote messages from partner servers.`,
+    options: [
+        {
+            name: `partner name`,
+            description: `The name of the partner server`,
+            type: `String`,
+            required: true
+        },{
+            name: `message`,
+            description: `The main body of the announcement message`,
+            type: `String`,
+            required: true
+        },{
+            name: `image url`,
+            description: `Optional URL to an image to be attached`,
+            type: `String`,
+            required: false
+        },
+    ],
     run: async(client, interaction, args) => {
 
-        args = [
-            {
-                name: `partner name`,
-                description: `The name of the partner server`,
-                type: `String`,
-                required: true
-            },{
-                name: `message`,
-                description: `The main body of the announcement message`,
-                type: `String`,
-                required: true
-            },{
-                name: `Partner Name`,
-                description: `The name of the partner server`,
-                type: `String`,
-                required: false
-            },
-        ]
+        const { value: PartnerName } = interaction.options.get('parner name');
+        const { value: MessageValue } = interaction.options.get('message');
+        const { value: imageUrl } = interaction.options.get('image url');
 
-        interaction.reply({ content: 'This command will eventually allow you to post partner messages. For now, this slash command is offline. Consider using the \`\`$partnermessage\`\` command instead.', ephemeral: true })
+        interaction.reply({ content: `This command will eventually allow you to post partner messages. For now, this slash command is offline. Consider using the \`\`$partnermessage\`\` command instead.\nPartnerName = ${PartnerName}\nMessageValue = ${MessageValue}\nimageUrl = ${imageUrl}`, ephemeral: true })
     }
 }
