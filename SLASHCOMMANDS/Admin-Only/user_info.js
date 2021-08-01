deaf211c6215f35ad689f51911f3d5e4c56d33a2
Liam Chambers <leemleem1234@outlook.com>
2021-08-01 14:17:34 -0400

    Forgetting I removed 'moment' from the file...

--- SLASHCOMMANDS/Admin-Only/user_info.js
+++ SLASHCOMMANDS/Admin-Only/user_info.js
@@ -1,6 +1,7 @@
 const discord = require('discord.js')
 const { CommandInteraction } = require('discord.js')
 const config = require ('../../config.json')
+const moment = require('moment');
 
 module.exports = {
     name: 'user_info',
