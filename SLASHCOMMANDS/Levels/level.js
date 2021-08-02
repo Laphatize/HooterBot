c2a58b9674168033b88d6e8954f88537ce4be7c4
Liam Chambers <leemleem1234@outlook.com>
2021-08-02 02:25:05 -0400

    Fixes to level command

--- SLASHCOMMANDS/Levels/level.js
+++ SLASHCOMMANDS/Levels/level.js
@@ -21,22 +21,23 @@ module.exports = {
         // GRABBING SLASH COMMAND INPUT VALUES
         const user = inputs[0];
 
-        console.log(`inputs[0] = ${inputs[0]}`)
-
 
         // INFO FOR USER ISSUING COMMAND
         if(!user) {
 
             const selfUser = await levels.fetch(interaction.user.id, interaction.guild.id)
             
+            console.log(`selfUser successfully fetched`)
+
             try {
+                console.log(`selfUser value not found`)
                 // IF USER INFO DNE
                 if (!selfUser) {
                     // CREATING EMBED FOR RESPONSE        
                     let levelUserNotFoundEmbed = new discord.MessageEmbed()
                         .setColor(config.embedRed)
                         .setTitle(`${config.emjREDTICK} Sorry!`)
-                        .setDescription(`I wasn't able to find your level information.`)
+                        .setDescription(`I wasn't able to find your level information. Be sure to post a message in the server first!`)
                         .setFooter(`If this is a bug, please submit a ModMail ticket to inform ${config.botAuthorUsername}.`)
     
                     // POST EMBED
@@ -47,23 +48,25 @@ module.exports = {
                 let infoEmbed = new discord.MessageEmbed()
                     .setColor(config.embedGrey)
                     .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
-                    .addField(`LEVEL:`, `${userLevels.level}`)
-                    .addField(`TOTAL XP`, `${userLevels.xp}`)
+                    .addField(`LEVEL:`, `${selfUser.level}`)
+                    .addField(`TOTAL XP`, `${selfUser.xp}`)
     
                 // POST EMBED
                 return interaction.reply({ embeds: [infoEmbed] })
             }
             catch (err) {
+                interaction.reply({ content: `I've encountered an error running this command. Please inform <@${config.botAuthorId}>.` })
                 console.log(err)
             }
         }
 
         // INFO FOR SPECIFIED USER
-        else {
-
+        if(user) {
+            console.log(`User value detected in command`)
             const targetUser = await levels.fetch(user, interaction.guild.id)
             
             try {
+                console.log(`User value not found`)
                 // IF USER INFO DNE
                 if (!targetUser) {
                     // CREATING EMBED FOR RESPONSE        
@@ -76,7 +79,22 @@ module.exports = {
                     // POST EMBED
                     return interaction.reply({ embeds: [levelUserNotFoundEmbed], ephemeral: true })
                 }
+
+                // FETCH GUILD MEMBER
+                interaction.guild.members.fetch(user)
+                .then(user => {
+                    // CREATING EMBED FOR RESPONSE        
+                    let infoEmbed = new discord.MessageEmbed()
+                        .setColor(config.embedGrey)
+                        .setAuthor(user.username, interaction.user.displayAvatarURL())
+                        .addField(`LEVEL:`, `${targetUser.level}`)
+                        .addField(`TOTAL XP`, `${targetUser.xp}`)
+        
+                    // POST EMBED
+                    return interaction.reply({ embeds: [infoEmbed], ephemeral: true })
+                })  
             } catch (err) {
+                interaction.reply({ content: `I've encountered an error running this command. Please inform <@${config.botAuthorId}>.` })
                 console.log(err);
             }
         }
