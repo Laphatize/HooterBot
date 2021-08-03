cce10d7ed05f753c37d6b95c85a7c0e7fa038404
Liam Chambers <leemleem1234@outlook.com>
2021-08-03 15:06:19 -0400

    Trying to fix logic issue with verif_pm

--- SLASHCOMMANDS/Mod-Only/verif_pm.js
+++ SLASHCOMMANDS/Mod-Only/verif_pm.js
@@ -19,9 +19,39 @@ module.exports = {
 
         // GRABBING SLASH COMMAND INPUT VALUES
         const message = inputs[0];
+        
+        // IF THE VERIF CHANNEL IS CLOSED OR ARCHIVED
+        if(interaction.channel.name.startsWith(`closed-`) || interaction.channel.name.startsWith(`archived-`)) {
+            // DEFINING EMBED
+            let normalMessages = new discord.MessageEmbed()
+                .setColor(config.embedRed)
+                .setTitle(`${config.emjREDTICK} Error!`)
+                .setDescription(`This ticket is completed (the channel name does not start with \`\`verify-\`\`), you can send messages in here normally.\n\nHere's the message you sent so you can copy/paste it in the chat:\n*${message}*`)
+                .setTimestamp()
+            
+            // SENDING MESSAGE
+            return interaction.reply({ embeds: [normalMessages], ephemeral: true })
+        }
+
 
         // IF NOT USED IN VERIFICATION CHANNEL
-        if(!interaction.channel.name.startsWith(`verify-`) || !interaction.channel.name.startsWith(`closed-`) || !interaction.channel.name.startsWith(`archived-`)) {
+        if(interaction.channel.name.startsWith(`verify-`)) {
+
+            await interaction.defer()
+
+            // GRABBING MESSAGE CONTENT AND FORMATTING FOR EMBED
+            let modAdminMsgEmbed = new discord.MessageEmbed()
+                .setColor(config.embedBlurple)
+                .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({ dynamic:true }))
+                .setDescription(message)
+                .setTimestamp()
+                .setFooter(`This is a Mods/Admins message and is not sent to the user.`)
+
+            await interaction.editReply({ embeds: [modAdminMsgEmbed] })
+        }
+
+
+        else {
             // DEFINING EMBED
             let wrongChannelEmbed = new discord.MessageEmbed()
                 .setColor(config.embedRed)
@@ -32,33 +62,5 @@ module.exports = {
             // SENDING MESSAGE
             return interaction.reply({ embeds: [wrongChannelEmbed], ephemeral: true })
         }
-
-        
-        // IF THE VERIF CHANNEL IS CLOSED OR ARCHIVED
-        if(interaction.channel.name.startsWith(`closed-`) || interaction.channel.name.startsWith(`archived-`)) {
-            // DEFINING EMBED
-            let normalMessages = new discord.MessageEmbed()
-                .setColor(config.embedRed)
-                .setTitle(`${config.emjREDTICK} Error!`)
-                .setDescription(`Because this ticket is completed (the channel name does not start with \`\`verify-\`\`), you can send messages in here normally.\n\nHere's the message you sent so you can copy/paste it in the chat:\n*${message}*`)
-                .setTimestamp()
-            
-            // SENDING MESSAGE
-            return interaction.reply({ embeds: [normalMessages], ephemeral: true })
-        }
-
-
-        
-        await interaction.defer()
-
-        // GRABBING MESSAGE CONTENT AND FORMATTING FOR EMBED
-        let modAdminMsgEmbed = new discord.MessageEmbed()
-            .setColor(config.embedBlurple)
-            .setAuthor(interaction.user.username, interaction.user.displayAvatarURL({ dynamic:true }))
-            .setDescription(message)
-            .setTimestamp()
-            .setFooter(`This is a Mods/Admins message and is not sent to the user.`)
-
-        await interaction.editReply({ embeds: [modAdminMsgEmbed] })
     }
 }
