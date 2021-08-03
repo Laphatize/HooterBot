a1bb97ac4819105f7cf4d1b0ccc683e8a083750a
Liam Chambers <leemleem1234@outlook.com>
2021-08-03 00:24:51 -0400

    Level Import command test

--- SLASHCOMMANDS/Levels/user_levelimport.js
+++ SLASHCOMMANDS/Levels/user_levelimport.js
@@ -4,10 +4,10 @@ const levels = require('discord-xp');
 
 module.exports = {
     name: 'user_levelimport',
-    description: `(ADMIN) Import MEE6 Leaderboard values.`,
+    description: `(ADMIN) Import MEE6 Leaderboard values for up to 5 users at a time.`,
     options: [
         {
-            name: `user`,
+            name: `user1`,
             description: `The user who's XP is being imported.`,
             type: `USER`,
             required: true
@@ -16,6 +16,46 @@ module.exports = {
             description: `The XP value the user currently has.`,
             type: `INTEGER`,
             required: true
+        },{
+            name: `user2`,
+            description: `The user who's XP is being imported.`,
+            type: `USER`,
+            required: false
+        },{
+            name: `xp_value`,
+            description: `The XP value the user currently has.`,
+            type: `INTEGER`,
+            required: false
+        },{
+            name: `user3`,
+            description: `The user who's XP is being imported.`,
+            type: `USER`,
+            required: false
+        },{
+            name: `xp_value`,
+            description: `The XP value the user currently has.`,
+            type: `INTEGER`,
+            required: false
+        },{
+            name: `user4`,
+            description: `The user who's XP is being imported.`,
+            type: `USER`,
+            required: false
+        },{
+            name: `xp_value`,
+            description: `The XP value the user currently has.`,
+            type: `INTEGER`,
+            required: false
+        },{
+            name: `user5`,
+            description: `The user who's XP is being imported.`,
+            type: `USER`,
+            required: false
+        },{
+            name: `xp_value`,
+            description: `The XP value the user currently has.`,
+            type: `INTEGER`,
+            required: false
         }
     ],
     permissions: 'ADMINISTRATOR',
@@ -24,8 +64,22 @@ module.exports = {
     run: async(client, interaction, inputs) => {
 
         // GRABBING SLASH COMMAND INPUT VALUES
-        const user = inputs[0];
-        const xpValue = inputs[1];
+        const user1 = inputs[0];
+        const xpValue1 = inputs[1];
+        const user2 = inputs[2];
+        const xpValue2 = inputs[3];
+        const user3 = inputs[4];
+        const xpValue3 = inputs[5];
+        const user4 = inputs[6];
+        const xpValue4 = inputs[7];
+        const user5 = inputs[8];
+        const xpValue5 = inputs[9];
+
+
+        // APPEND XP VALUE FOR USER
+        levels.appendXp(user.id, interaction.guild.id, xpValue);
+
+
 
 
 
