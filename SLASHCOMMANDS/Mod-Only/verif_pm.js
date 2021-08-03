9384f8df723ed68f8b6f55ab3f46a1d456c67088
Liam Chambers <leemleem1234@outlook.com>
2021-08-03 14:26:04 -0400

    Testing new slash command

---
+++ SLASHCOMMANDS/Mod-Only/verif_pm.js
@@ -0,0 +1,34 @@
+const discord = require('discord.js')
+const config = require ('../../config.json')
+
+module.exports = {
+    name: 'verif_pm',
+    description: `(MODERATOR) Send a message in a ticket channel without it being sent to the user.`,
+    options: [
+        {
+            name: `message`,
+            description: `Your message for the channel.`,
+            type: `STRING`,
+            required: true,
+        },
+    ],
+    permissions: 'MANAGE_MESSAGES', //ADMINISTRATOR
+    cooldown: 0,
+    defaultPermission: true,
+    run: async(client, interaction, inputs) => {
+
+        // GRABBING SLASH COMMAND INPUT VALUES
+        const message = inputs[0];
+
+        // GRABBING MESSAGE CONTENT AND FORMATTING FOR EMBED
+        let modAdminMsgEmbed = new discord.MessageEmbed()
+            .setColor(config.embedBlurple)
+            .setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic:true }))
+            .setTitle(`For Mods/Admins only:`)
+            .setDescription(message.content)
+            .setTimestamp()
+
+
+        interaction.return({ embeds: [modAdminMsgEmbed] })
+    }
+}
