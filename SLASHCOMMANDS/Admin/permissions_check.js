58670f4c52215e9d917160cc6796590bf1a162c3
Liam Chambers <leemleem1234@outlook.com>
2021-08-04 18:26:17 -0400

    Setup for general bot perms

--- SLASHCOMMANDS/Admin/permissions_check.js
+++ SLASHCOMMANDS/Admin/permissions_check.js
@@ -3,12 +3,17 @@ const config = require ('../../config.json')
 
 module.exports = {
     name: 'permissions_check',
-    description: `ADMIN | Checks HooterBot's general permissions or for a specified channel.`,
+    description: `ADMIN | Checks HooterBot's permissions OR the permissions of a specific role in a channel.`,
     options: [
         {
             name: `channel`,
             description: `Name of the channel to check HooterBot's permissions`,
             type: `CHANNEL`,
+            required: false
+        }, {
+            name: `role`,
+            description: `Specify the role to use for checking permissions. `,
+            type: `ROLE`,
             required: false
         }
     ],
@@ -17,26 +22,105 @@ module.exports = {
     defaultPermission: true,
     run: async(client, interaction, inputs) => {
 
-        if(!inputs[0]) {
-            // HOOTERBOT'S GENERAL PERMISSIONS
-            let permissionsArray = interaction.guild.me.permissions.toArray()
-            let permsHave = [];
+        const generalPermsArray = [
+            'ADMINISTRATOR',
+            'MANAGE_CHANNELS',
+            'MANAGE_ROLES',
+            'MANAGE_EMOJIS',
+            'VIEW_AUDIT_LOG',
+            'VIEW_GUILD_INSIGHTS',
+            'MANAGE_WEBHOOKS',
+            'MANAGE_GUILD',
+            'CREATE_INSTANT_INVITE',
+            'CHANGE_NICKNAME',
+            'MANAGE_NICKNAMES',
+            'KICK_MEMBERS',
+            'BAN_MEMBERS',
+        ]
 
-            for (const permission of permissionsArray) {
-                permsHave.push(`${config.emjGREENTICK} \`\`${permission}\`\``)
+        const textPermsArray = [
+            'VIEW_CHANNEL',
+            'SEND_MESSAGES', 
+            'EMBED_LINKS',
+            'ATTACH_FILES',
+            'ADD_REACTIONS',
+            'USE_EXTERNAL_EMOJIS',
+            'MENTION_EVERYONE',
+            'MANAGE_MESSAGES',
+            'READ_MESSAGE_HISTORY',
+            'SEND_TTS_MESSAGES',
+            'USE_APPLICATION_COMMANDS',
+        ]
+
+
+        const voicePermsArray = [
+            'VIEW_CHANNEL',
+            'CONNECT',
+            'SPEAK',
+            'STREAM',
+            'USE_VAD',
+            'PRIORITY_SPEAKER',
+            'MUTE_MEMBERS',
+            'DEAFEN_MEMBERS',
+            'MOVE_MEMBERS',
+            'REQUEST_TO_SPEAK'
+        ]
+
+        const channel = inputs[0]
+        const role = inputs[1]
+
+
+
+        if(!channel && !role) {
+            // CHECK HOOTERBOT'S GENERAL PERMISSIONS
+            let generalPermsHave = [];
+
+            // CHECKING GENERAL PERMS
+            for (const permission of generalPermsArray) {
+                if(interaction.guild.me.permissions.has(permission)) {
+                    generalPermsHave.push(`${config.emjGREENTICK} \`\`${permission}\`\``)
+                }
+                else {
+                    generalPermsHave.push(`${config.emjREDTICK} \`\`${permission}\`\``)
+                }
             }
-            
-            if (permsHave.length === 0 ) {
-                permsHave.push(`${config.emjREDTICK} \`\`No Permissions Enabled\`\``)
+
+
+            // CHECK HOOTERBOT'S TEXT PERMISSIONS
+            let textPermsHave = [];
+
+            // CHECKING GENERAL PERMS
+            for (const permission of textPermsArray) {
+                if(interaction.guild.me.permissions.has(permission)) {
+                    textPermsHave.push(`${config.emjGREENTICK} \`\`${permission}\`\``)
+                }
+                else {
+                    textPermsHave.push(`${config.emjREDTICK} \`\`${permission}\`\``)
+                }
             }
 
 
+            // CHECK HOOTERBOT'S TEXT PERMISSIONS
+            let voicePermsHave = [];
+
+            // CHECKING GENERAL PERMS
+            for (const permission of voicePermsArray) {
+                if(interaction.guild.me.permissions.has(permission)) {
+                    voicePermsHave.push(`${config.emjGREENTICK} \`\`${permission}\`\``)
+                }
+                else {
+                    voicePermsHave.push(`${config.emjREDTICK} \`\`${permission}\`\``)
+                }
+            }
+
+
+            // GENERATING EMBED TO NOTE THE PERMISSIONS
             let logPerms = new discord.MessageEmbed()
                 .setColor(config.embedBlurple)
-                .setTitle(`${config.botName}'s General Discord Permisisons:`)
-                .setDescription(`**PERMISSIONS**\n${permsHave.join(`\n`)}`)
-                .addField(`INTENTS:`, `${config.emjGREENTICK} \`\`GUILDS\`\`\n${config.emjGREENTICK} \`\`GUILD_MEMBERS\`\`\n${config.emjGREENTICK} \`\`GUILD_MESSAGES\`\`\n${config.emjGREENTICK} \`\`DIRECT_MESSAGES\`\``, true)
-                .addField(`PARTIALS:`, `${config.emjGREENTICK} \`\`CHANNEL\`\`\n${config.emjGREENTICK} \`\`MESSAGE\`\``, true)
+                .setTitle(`${config.botName}'s Discord Permissions:`)
+                .addField(`General Permissions:`,`${generalPermsHave.join(`\n`)}`)
+                .addField(`Text Permissions:`,`${textPermsHave.join(`\n`)}`)
+                .addField(`Voice Permissions:`,`${voicePermsHave.join(`\n`)}`)
                 .setTimestamp()
 
             return interaction.reply({ embeds: [logPerms] })
