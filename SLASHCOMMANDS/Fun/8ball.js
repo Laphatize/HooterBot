b9b76da71bf559b7d189dabfe251ca96cbc8b727
Liam Chambers <leemleem1234@outlook.com>
2021-08-04 00:33:39 -0400

    Dynamic embed color based on response
    Good will turn green
    Neutral will turn blurple
    Bad will turn red

--- SLASHCOMMANDS/Fun/8ball.js
+++ SLASHCOMMANDS/Fun/8ball.js
@@ -27,7 +27,7 @@ module.exports = {
 
         // INITIAL RESPONSE
         let eightBallInitialEmbed = new discord.MessageEmbed()
-            .setColor(config.embedTempleRed)
+            .setColor(config.embedDarkGrey)
             .addField(`${interaction.user.username} asks:`, `*"${message}"*
             \n${config.emjThinking} *The Magic 8 Ball is being shaken...*`)
 
@@ -36,32 +36,33 @@ module.exports = {
 
         // ARRAY OF OUTCOMES AND FILENAMES
         const outcomesArray =  [
-            [`ASK AGAIN LATER`, `https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Ask%20Again%20Later.png`],
-            [`ASK AGAIN`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Ask%20Again.png`],
-            [`CANNOT TELL`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Cannot%20Tell.png`],
-            [`COUNT ON IT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Count%20On%20It.png`],
-            [`GO FOR IT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Go%20For%20It.png`],
-            [`MAYBE`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Maybe.png`],
-            [`MY REPLY IS NO`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_My%20Reply%20Is%20No.png`],
-            [`MY REPLY IS YES`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_My%20Reply%20Is%20Yes.png`],
-            [`NO DOUBT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_No%20Doubt.png`],
-            [`NO`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_No.png`],
-            [`NOT LIKELY`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Likely.png`],
-            [`NOT LOOKING GOOD...`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Looking%20Good.png`],
-            [`NOT NOW`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Now.png`],
-            [`NOT TODAY`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Today.png`],
-            [`TRY ASKING A DIFFERENT MAGIC 8 BALL`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Try%20Asking%20A%20Different%20Magic%208%20Ball.png`],
-            [`VERY LIKELY`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Very%20Likely.png`],
-            [`VERY MUCH DOUBT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Very%20Much%20Doubt.png`],
-            [`WAIT FOR IT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Wait%20For%20It.png`],
-            [`WHY ARE YOU ASKING ME???`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Why%20Are%20You%20Asking%20Me.png`],
-            [`YES`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Yes.png`]
+            [`ASK AGAIN LATER`, `https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Ask%20Again%20Later.png`, config.embedBlurple],
+            [`ASK AGAIN`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Ask%20Again.png`, config.embedBlurple],
+            [`CANNOT TELL`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Cannot%20Tell.png`, config.embedBlurple],
+            [`COUNT ON IT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Count%20On%20It.png`, config.embedGreen],
+            [`GO FOR IT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Go%20For%20It.png`, config.embedGreen],
+            [`MAYBE`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Maybe.png`, config.embedBlurple],
+            [`MY REPLY IS NO`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_My%20Reply%20Is%20No.png`, config.embedRed],
+            [`MY REPLY IS YES`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_My%20Reply%20Is%20Yes.png`, config.embedGreen],
+            [`NO DOUBT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_No%20Doubt.png`, config.embedGreen],
+            [`NO`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_No.png`, config.embedRed],
+            [`NOT LIKELY`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Likely.png`, config.embedRed],
+            [`NOT LOOKING GOOD...`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Looking%20Good.png`, config.embedRed],
+            [`NOT NOW`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Now.png`, config.embedBlurple],
+            [`NOT TODAY`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Not%20Today.png`, config.embedBlurple],
+            [`TRY ASKING A DIFFERENT MAGIC 8 BALL`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Try%20Asking%20A%20Different%20Magic%208%20Ball.png`, config.embedBlurple],
+            [`VERY LIKELY`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Very%20Likely.png`, config.embedGreen],
+            [`VERY MUCH DOUBT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Very%20Much%20Doubt.png`, config.embedRed],
+            [`WAIT FOR IT`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Wait%20For%20It.png`, config.embedBlurple],
+            [`WHY ARE YOU ASKING ME???`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Why%20Are%20You%20Asking%20Me.png`, config.embedBlurple],
+            [`YES`,`https://raw.githubusercontent.com/MrMusicMan789/HooterBot/main/SLASHCOMMANDS/Fun/8ball_Images/8ball_Yes.png`, config.embedGreen]
         ]
 
         // PICKING RANDOM RESPONSE VALUE AND GETTING TEXT + IMAGE
         let randomIndex = Math.floor(Math.random() * outcomesArray.length);
         let textResponse = outcomesArray[randomIndex][0]
         let imageResponse = outcomesArray[randomIndex][1]
+        let embedColor = outcomesArray[randomIndex][2]
 
 
         console.log(`randomIndex = ${randomIndex}`)
@@ -70,7 +71,7 @@ module.exports = {
 
 
         let eightBallFinalEmbed = new discord.MessageEmbed()
-            .setColor(config.embedTempleRed)
+            .setColor(embedColor)
             .addField(`${interaction.user.username} asks:`, `*"${message}"*`)
             .addField(`\nThe Magic 8 Ball says...`, `***${textResponse}***`)
             .setImage(imageResponse)
