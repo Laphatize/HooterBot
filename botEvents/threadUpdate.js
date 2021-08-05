466affc88324541da1545654f88f0359a96672de
Liam Chambers <leemleem1234@outlook.com>
2021-08-05 13:46:09 -0400

    Resorting logging to basic "thread update"

--- botEvents/threadUpdate.js
+++ botEvents/threadUpdate.js
@@ -28,94 +28,23 @@ module.exports = {
         var threadArchiveTimeStampNoAdjust = moment(thread.archiveTimestamp).add(thread.autoArchiveDuration, 'minutes').utcOffset(-4).format("LLLL")
 
 
-        // THREAD AUTO-ARCHIVED
-        if(!thread.archived && thread.locked && (threadArchiveTimeStampNoAdjust == moment(Date.now()).utcOffset(-4).format("LLLL"))) {
-            // GENERATE NOTICE EMBED
-            let threadLogEntry = new discord.MessageEmbed()
-                .setColor(config.embedGrey)
-                .setTitle(`Thread Auto-Archived`)
-                .addField(`Thread:`, `${thread}`, true)
-                .addField(`Thread ID:`, `${thread.id}`, true)
-                .addField(`Thread Type:`, `${thread.type}`, true)
-                .addField(`Parent Channel:`, `<#${thread.parent.id}>`, true)
-                .addField(`Creator:`, `<@${thread.ownerId}>`, true)
-                .addField(`Creator ID:`, `${thread.ownerId}`, true)
-                .addField(`Scheduled Close:`, `After *${threadDurationTimeString}* of inactivity.`)
-                .addField(`Archived?`, `${!thread.archived}`, true)
-                .addField(`Locked?`, `${!thread.locked}`, true)
-                .setTimestamp()
+        // GENERATE NOTICE EMBED
+        let threadLogEntry = new discord.MessageEmbed()
+            .setColor(config.embedGrey)
+            .setTitle(`Thread Updated`)
+            .addField(`Thread:`, `${thread}`, true)
+            .addField(`Thread ID:`, `${thread.id}`, true)
+            .addField(`Thread Type:`, `${thread.type}`, true)
+            .addField(`Parent Channel:`, `<#${thread.parent.id}>`, true)
+            .addField(`Creator:`, `<@${thread.ownerId}>`, true)
+            .addField(`Creator ID:`, `${thread.ownerId}`, true)
+            .addField(`Scheduled Close:`, `After *${threadDurationTimeString}* of inactivity.`)
+            .addField(`Archived?`, `${!thread.archived}`, true)
+            .addField(`Locked?`, `${!thread.locked}`, true)
+            .setTimestamp()
 
-            // FETCHING LOG CHANNEL AND SENDING CREATION NOTICE
-            modLogChannel.send({ content: '!thread.archived && thread.locked && (date check)', embeds: [threadLogEntry] })
-                .catch(err => console.log(err))
-        }
-
-
-        // THREAD LOCKED BY ADMIN/MOD
-        if(!thread.archived && !thread.locked) {
-            // GENERATE NOTICE EMBED
-            let threadLogEntry = new discord.MessageEmbed()
-                .setColor(config.embedGrey)
-                .setTitle(`Thread Locked by Server Staff`)
-                .addField(`Thread:`, `${thread}`, true)
-                .addField(`Thread ID:`, `${thread.id}`, true)
-                .addField(`Thread Type:`, `${thread.type}`, true)
-                .addField(`Parent Channel:`, `<#${thread.parent.id}>`, true)
-                .addField(`Creator:`, `<@${thread.ownerId}>`, true)
-                .addField(`Creator ID:`, `${thread.ownerId}`, true)
-                .addField(`Archived?`, `${!thread.archived}`, true)
-                .addField(`Locked?`, `${!thread.locked}`, true)
-                .setTimestamp()
-
-            // FETCHING LOG CHANNEL AND SENDING CREATION NOTICE
-            modLogChannel.send({ content: '!thread.archived && !thread.locked', embeds: [threadLogEntry] })
-                .catch(err => console.log(err))
-        }
-
-
-        // THREAD UNLOCKED BY ADMIN/MOD
-        if(thread.archived && !thread.locked) {
-            // GENERATE NOTICE EMBED
-            let threadLogEntry = new discord.MessageEmbed()
-                .setColor(config.embedGrey)
-                .setTitle(`Thread Unlocked by Server Staff`)
-                .addField(`Thread:`, `${thread}`, true)
-                .addField(`Thread ID:`, `${thread.id}`, true)
-                .addField(`Thread Type:`, `${thread.type}`, true)
-                .addField(`Parent Channel:`, `<#${thread.parent.id}>`, true)
-                .addField(`Creator:`, `<@${thread.ownerId}>`, true)
-                .addField(`Creator ID:`, `${thread.ownerId}`, true)
-                .addField(`Scheduled Close:`, `After *${threadDurationTimeString}* of inactivity.`)
-                .addField(`Archived?`, `${thread.archived}`, true)
-                .addField(`Locked?`, `${!thread.locked}`, true)
-                .setTimestamp()
-
-            // FETCHING LOG CHANNEL AND SENDING CREATION NOTICE
-            modLogChannel.send({ content: 'thread.archived && !thread.locked', embeds: [threadLogEntry] })
-                .catch(err => console.log(err))
-        }
-
-        // THREAD REOPENED BY USER
-        if(thread.archived && thread.locked) {
-            // GENERATE NOTICE EMBED
-            let threadLogEntry = new discord.MessageEmbed()
-                .setColor(config.embedGrey)
-                .setTitle(`Thread Unlocked by Server Staff`)
-                .addField(`Thread:`, `${thread}`, true)
-                .addField(`Thread ID:`, `${thread.id}`, true)
-                .addField(`Thread Type:`, `${thread.type}`, true)
-                .addField(`Parent Channel:`, `<#${thread.parent.id}>`, true)
-                .addField(`Creator:`, `<@${thread.ownerId}>`, true)
-                .addField(`Creator ID:`, `${thread.ownerId}`, true)
-                .addField(`Scheduled Close:`, `After *${threadDurationTimeString}* of inactivity.`)
-                .addField(`Archived?`, `${thread.archived}`, true)
-                .addField(`Locked?`, `${!thread.locked}`, true)
-                .setTimestamp()
-
-            // FETCHING LOG CHANNEL AND SENDING CREATION NOTICE
-            modLogChannel.send({ content: 'thread.archived && thread.locked', embeds: [threadLogEntry] })
-                .catch(err => console.log(err))
-        }
-        
+        // FETCHING LOG CHANNEL AND SENDING CREATION NOTICE
+        modLogChannel.send({ embeds: [threadLogEntry] })
+            .catch(err => console.log(err))
 	},
 };
