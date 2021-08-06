b4037395bb2c81eb3a604bf2adce530b8d73a5e8
Liam Chambers <leemleem1234@outlook.com>
2021-08-06 02:11:53 -0400

    Message edit formatting

--- botEvents/messageUpdate.js
+++ botEvents/messageUpdate.js
@@ -13,9 +13,7 @@ module.exports = {
             .setColor(config.embedGrey)
             .setTitle(`Message Updated`)
             .setAuthor(oldMessage.author.tag, oldMessage.author.displayAvatarURL({ dynamic:true }))
-            .setDescription(`Channel: ${oldMessage.channel}
-                \nOld Message: ${oldMessage.content}
-                \nNew Message`, `${newMessage.content}`)
+            .setDescription(`Channel: ${oldMessage.channel}\nOld Message: ${oldMessage.content}\nNew Message`, `${newMessage.content}`)
             .setTimestamp()
 
         // LOG ENTRY
