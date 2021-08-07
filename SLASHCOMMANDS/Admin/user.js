b6d7845c2ba72f047141332c6b1896a05cd7d3a5
Liam Chambers <leemleem1234@outlook.com>
2021-08-06 20:58:28 -0400

    Name fix, trying to fix perm loading

--- SLASHCOMMANDS/Admin/user.js
+++ SLASHCOMMANDS/Admin/user.js
@@ -11,7 +11,7 @@ module.exports = {
             type: 'SUB_COMMAND',
             description: `ADMIN | Import MEE6 Leaderboard values for up to 10 users at a time.`,
         },{
-            name: 'levelimport',
+            name: 'info',
             type: 'SUB_COMMAND',
             description: `MODERATOR | A command for generating information about a specific user in the server.`,
         }
--- bot.js
+++ bot.js
@@ -117,10 +117,12 @@ client.on('ready', async () => {
     console.log(`======================================\n`);
 
     // GUILD SLASH COMMANDS - MMM789 TEST
-    await client.guilds.cache.get('530503548937699340').commands.set(arrayOfSlashCmds)?.commands.permissions.set({ testServerPermArray })         //  .commands.set([]) to empty
+    await client.guilds.cache.get('530503548937699340').commands.set(arrayOfSlashCmds)
+    await client.guilds.cache.get('530503548937699340')?.commands.permissions.set({ testServerPermArray })         //  .commands.set([]) to empty
 
     // GUILD SLASH COMMANDS - MMM789 2ND TEST
-    await client.guilds.cache.get('859798908841230367').commands.set(arrayOfSlashCmds)?.commands.permissions.set({ permissionsArray })          //  .commands.set([]) to empty
+    await client.guilds.cache.get('859798908841230367').commands.set(arrayOfSlashCmds)
+    await client.guilds.cache.get('859798908841230367')?.commands.permissions.set({ permissionsArray })          //  .commands.set([]) to empty
 
     // // GUILD SLASH COMMANDS - TU SERVER ID
     // await client.guilds.cache.get('829409161581821992').commands.set(arrayOfSlashCmds)?.commands.permissions.set({ templeServerPermArray })          //  .commands.set([]) to empty
