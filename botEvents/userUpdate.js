ee8c66e74e2cf7209a505027c092c1ba0c3ddb78
Liam Chambers <leemleem1234@outlook.com>
2021-08-09 19:37:48 -0400

    Commenting out User Update

--- botEvents/userUpdate.js
+++ botEvents/userUpdate.js
@@ -8,17 +8,17 @@ module.exports = {
         // LOG CHANNEL
         const modLogChannel = oldUser.guild.channels.cache.find(ch => ch.name === `mod-log`)
 
-        // LOG EMBED
-        let logEmbed = new discord.MessageEmbed()
-            .setColor(config.embedGrey)
-            .setTitle(`User Updated`)
-        //     .addField(`User:`, `${member}`, true)
-        //     .addField(`Tag:`, `${member.user.tag}`, true)
-        //     .addField(`ID:`, `${member.id}`, true)
-        //     .addField(`Time in server:`, `${memberDuration}`)
-            .setTimestamp()
+        // // LOG EMBED
+        // let logEmbed = new discord.MessageEmbed()
+        //     .setColor(config.embedGrey)
+        //     .setTitle(`User Updated`)
+        // //     .addField(`User:`, `${member}`, true)
+        // //     .addField(`Tag:`, `${member.user.tag}`, true)
+        // //     .addField(`ID:`, `${member.id}`, true)
+        // //     .addField(`Time in server:`, `${memberDuration}`)
+        //     .setTimestamp()
 
-        // LOG ENTRY
-        modLogChannel.send({embeds: [logEmbed]})
+        // // LOG ENTRY
+        // modLogChannel.send({embeds: [logEmbed]})
 	},
 };
