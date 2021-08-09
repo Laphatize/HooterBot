e411e4d048e74a08747dbad44fd333dea6c60fe3
Liam Chambers <leemleem1234@outlook.com>
2021-08-09 17:16:47 -0400

    Moving commands to avoid duplicates

--- Old Command Files/verif_perksembed.js
+++ Old Command Files/verif_perksembed.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js')
-const config = require ('../../config.json')
-const guildSchema = require('../../Database/guildSchema');
+const config = require ('../config.json')
+const guildSchema = require('../Database/guildSchema');
 
 
 module.exports = {
