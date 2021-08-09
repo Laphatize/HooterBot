2531953cdc3a3feb158869f29b94f867e198b330
Liam Chambers <leemleem1234@outlook.com>
2021-08-08 21:28:16 -0400

    Commenting out test

--- SLASHCOMMANDS/Admin/user.js
+++ SLASHCOMMANDS/Admin/user.js
@@ -23,125 +23,126 @@ module.exports = {
                     type: `INTEGER`,
                     required: true
                 }]
-        },{
-            // LEVEL IMPORT
-            name: `levelimport`,
-            description: `ADMIN | Import MEE6 Leaderboard values for up to 10 users at a time.`,
-            type: 'SUB_COMMAND',
-            options: [{
-                    name: `user1`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: true
-                },{
-                    name: `xp_value1`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: true
-                },{
-                    name: `user2`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value2`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user3`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value3`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user4`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value4`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user5`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value5`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user6`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value6`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user7`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value7`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user8`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value8`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user9`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value9`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                },{
-                    name: `user10`,
-                    description: `The user who's XP is being imported.`,
-                    type: `USER`,
-                    required: false
-                },{
-                    name: `xp_value10`,
-                    description: `The XP value the user currently has.`,
-                    type: `INTEGER`,
-                    required: false
-                }],
-        },{
-            // LEVEL IMPORT
-            name: `info`,
-            description: `MODERATOR | A command for generating information about a specific user in the server.`,
-            type: 'SUB_COMMAND',
-            options: [
-                {
-                    name: `member`,
-                    description: `The user to generate information about.`,
-                    type: `USER`,
-                    required: true
-                },
-            ],
+                
+        // },{
+        //     // LEVEL IMPORT
+        //     name: `levelimport`,
+        //     description: `ADMIN | Import MEE6 Leaderboard values for up to 10 users at a time.`,
+        //     type: 'SUB_COMMAND',
+        //     options: [{
+        //             name: `user1`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: true
+        //         },{
+        //             name: `xp_value1`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: true
+        //         },{
+        //             name: `user2`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value2`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user3`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value3`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user4`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value4`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user5`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value5`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user6`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value6`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user7`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value7`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user8`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value8`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user9`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value9`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         },{
+        //             name: `user10`,
+        //             description: `The user who's XP is being imported.`,
+        //             type: `USER`,
+        //             required: false
+        //         },{
+        //             name: `xp_value10`,
+        //             description: `The XP value the user currently has.`,
+        //             type: `INTEGER`,
+        //             required: false
+        //         }],
+        // },{
+        //     // LEVEL IMPORT
+        //     name: `info`,
+        //     description: `MODERATOR | A command for generating information about a specific user in the server.`,
+        //     type: 'SUB_COMMAND',
+        //     options: [
+        //         {
+        //             name: `user`,
+        //             description: `The user to generate information about.`,
+        //             type: `USER`,
+        //             required: true
+        //         },
+        //     ],
         },
     ],
     permissions: 'MANAGE_MESSAGES',
