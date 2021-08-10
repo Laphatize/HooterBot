2032e5e86a889a1266608fa6efe71ca126b3bf0e
Liam Chambers <leemleem1234@outlook.com>
2021-08-10 01:44:48 -0400

    Expiration date

--- botEvents/inviteCreate.js
+++ botEvents/inviteCreate.js
@@ -8,19 +8,21 @@ module.exports = {
         // LOG CHANNEL
         const modLogChannel = invite.guild.channels.cache.find(ch => ch.name === `mod-log`)
 
-        
-        let maxUse = invite.maxUses;
+        let expirationDate = invite.expiresAt
+        if(invite.expiresAt == null) expirationDate == `*(infinite)*`
 
+
+        let maxUse = invite.maxUses;
         if(invite.maxUses == 0) maxUse = `*(infinite)*`
 
         // LOG EMBED
         let logEmbed = new discord.MessageEmbed()
             .setColor(config.embedGreen)
             .setTitle(`Server Invite Created`)
-            .setDescription(`**Invite URL:** ${invite.url}\n**Creator:** ${invite.inviter}\n**Expiration:** ${invite.expiresAt}\n**Max Uses:** ${maxUse}\n**Channel:** ${invite.channel}`)
+            .setDescription(`**Invite URL:** ${invite.url}\n**Creator:** ${invite.inviter}\n**Expiration:** ${expirationDate}\n**Max Uses:** ${maxUse}\n**Channel:** ${invite.channel}`)
             .setTimestamp()
 
         // LOG ENTRY
         modLogChannel.send({embeds: [logEmbed]})
-	},
+    },
 };
