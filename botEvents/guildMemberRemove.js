ce8070788490c3f0cc5131b1fed09c305911aba1
Liam Chambers <leemleem1234@outlook.com>
2021-08-09 21:31:00 -0400

    user leave auto ticket close fix

--- botEvents/guildMemberRemove.js
+++ botEvents/guildMemberRemove.js
@@ -37,104 +37,104 @@ module.exports = {
 
         // TICKET OPEN
         if(dbTicketData) {
-            member.guild.members.fetch(dbTicketData.CREATOR_ID)
-            .then(dmUser => {
+            let leftUser = member.guild.members.fetch(dbTicketData.CREATOR_ID)
+                .then(dmUser => {
 
-                // DELETING DATABASE ENTRY
-                ticketSchema.deleteOne({
-                    CREATOR_ID: dmUser.id
-                }).exec();
-
-
-                // LOGGING TICKET CLOSURE
-                let logCloseTicketEmbed = new discord.MessageEmbed()
-                    .setColor(config.embedOrange)
-                    .setTitle(`${config.emjORANGETICK} Verification Ticket Closed`)
-                    .addField(`User:`, `${dmUser}`, true)
-                    .addField(`User ID:`, `${dmUser.id}`, true)
-                    .addField(`Verified?`, `\`\` NO \`\``, true)
-                    .addField(`Ticket closed by:`, `<@${config.botId}> ***(automatically)***`)
-                    .setTimestamp()
-                
-
-                // SENDING LOG ENTRY
-                member.guild.channels.cache.find(ch => ch.name === `mod-log`).send({ embeds: [logCloseTicketEmbed] })
-                    .catch(err => console.log(err))
-
-                
-                // CLOSURE NOTICE TO CHANNEL
-                let closeNotice = new discord.MessageEmbed()
-                    .setColor(config.embedOrange)
-                    .setTitle(`${config.emjORANGETICK} User Left Server`)
-                    .setDescription(`**<@${config.botId}>** has automatically closed this ticket since the user has left the server. This message constitutes as the last message of the transcript; the DM-channel communications with the user have been severed.\n\nIf the contents of this ticket do not need to be archived for moderation actions, press \`\`Confirm Ticket Close\`\` to **permanently delete this channel *immediately***.\n\nIf this channel needs to be archived for moderation actions, press \`\`Do Not Close\`\` to keep this channel.`)
-                    .setTimestamp()
+                    // DELETING DATABASE ENTRY
+                    ticketSchema.deleteOne({
+                        CREATOR_ID: dmUser.id
+                    }).exec();
+                })
 
 
-                // BUTTONS
-                let InfoButton = new MessageButton()
-                    .setLabel("Confirm Ticket Close")
-                    .setStyle("SUCCESS")
-                    .setCustomId("Confirm_Ticket_Close")
-                let QuitButton = new MessageButton()
-                    .setLabel("Do Not Close")
-                    .setStyle("DANGER")
-                    .setCustomId("Ticket_DoNotClose")
+            // LOGGING TICKET CLOSURE
+            let logCloseTicketEmbed = new discord.MessageEmbed()
+                .setColor(config.embedOrange)
+                .setTitle(`${config.emjORANGETICK} Verification Ticket Closed`)
+                .addField(`User:`, `${leftUser}`, true)
+                .addField(`User ID:`, `${leftUser.id}`, true)
+                .addField(`Verified?`, `\`\` NO \`\``, true)
+                .addField(`Ticket closed by:`, `<@${config.botId}> ***(automatically)***`)
+                .setTimestamp()
+            
+
+            // SENDING LOG ENTRY
+            member.guild.channels.cache.find(ch => ch.name === `mod-log`).send({ embeds: [logCloseTicketEmbed] })
+                .catch(err => console.log(err))
+
+            
+            // CLOSURE NOTICE TO CHANNEL
+            let closeNotice = new discord.MessageEmbed()
+                .setColor(config.embedOrange)
+                .setTitle(`${config.emjORANGETICK} User Left Server`)
+                .setDescription(`**<@${config.botId}>** has automatically closed this ticket since the user has left the server. This message constitutes as the last message of the transcript; the DM-channel communications with the user have been severed.\n\nIf the contents of this ticket do not need to be archived for moderation actions, press \`\`Confirm Ticket Close\`\` to **permanently delete this channel *immediately***.\n\nIf this channel needs to be archived for moderation actions, press \`\`Do Not Close\`\` to keep this channel.`)
+                .setTimestamp()
 
 
-                // BUTTON ROW
-                let TicketCloseReviewButtonRow = new MessageActionRow()
-                .addComponents(
-                    InfoButton,
-                    QuitButton
-                );
+            // BUTTONS
+            let InfoButton = new MessageButton()
+                .setLabel("Confirm Ticket Close")
+                .setStyle("SUCCESS")
+                .setCustomId("Confirm_Ticket_Close")
+            let QuitButton = new MessageButton()
+                .setLabel("Do Not Close")
+                .setStyle("DANGER")
+                .setCustomId("Ticket_DoNotClose")
 
 
-                console.log(`dmUser = ${dmUser}`)
-                console.log(`dmUser.user = ${dmUser.user}`)
-                console.log(`dmUser.username = ${dmUser.username}`)
-                console.log(`dmUser.user.username = ${dmUser.user.username}`)
+            // BUTTON ROW
+            let TicketCloseReviewButtonRow = new MessageActionRow()
+            .addComponents(
+                InfoButton,
+                QuitButton
+            );
 
-                // // FETCHING TICKET CHANNEL AND SENDING CLOSURE NOTICE
-                // member.guild.channels.cache.find(ch => ch.name === `verify-${dmUser}`).send({ embeds: [closeNotice], components: [TicketCloseReviewButtonRow] })
-                //     .then(msg => {
-                //         // CHANGING TICKET CHANNEL NAME TO "closed-(username)" TO CUT DM-CHANNEL COMMS
-                //         msg.channel.setName(`closed-${dmUser.username.toLowerCase()}`)
 
-                //                 // EDIT THE INITIAL TICKET MESSAGE TO DISABLE BUTTON
-                //             // GRAB TICKET CHANNEL
-                //             initialChMsg = client.channels.cache.find(ch => ch.name === `closed-${dmUser.username.toLowerCase()}`)
-                //                 .then(ch => {
-                //                     // GRABBING THE INITIAL MESSAGE FROM TICKET CHANNEL
-                //                     msg = ch.messages.fetch(dbTicketData.TICKETCH1_MSG_ID)
+            console.log(`leftUser = ${leftUser}`)
+            console.log(`leftUser.user = ${leftUser.user}`)
+            console.log(`leftUser.username = ${leftUser.username}`)
+            console.log(`leftUser.user.username = ${leftUser.user.username}`)
 
-                //                     // CREATE INTRO EMBED FOR ADMIN/MOD TICKET CHANNEL
-                //                     let newTicketEditedEmbed = new discord.MessageEmbed()
-                //                         .setColor(config.embedGreen)
-                //                         .setTitle(`**Verification Ticket Closed**`)
-                //                         .addField(`User:`, `${dmUser}`, true)
-                //                         .addField(`User Tag:`, `${dmUser.tag}`, true)
-                //                         .addField(`User ID:`, `${dmUser.id}`, true)
-                //                         .setDescription(`*This ticket has been closed because the user has left the server.*`)
+            // // FETCHING TICKET CHANNEL AND SENDING CLOSURE NOTICE
+            // member.guild.channels.cache.find(ch => ch.name === `verify-${dmUser}`).send({ embeds: [closeNotice], components: [TicketCloseReviewButtonRow] })
+            //     .then(msg => {
+            //         // CHANGING TICKET CHANNEL NAME TO "closed-(username)" TO CUT DM-CHANNEL COMMS
+            //         msg.channel.setName(`closed-${dmUser.username.toLowerCase()}`)
 
-                //                     let QuitButton = new MessageButton()
-                //                         .setLabel("End Verification")
-                //                         .setStyle("DANGER")
-                //                         .setCustomId("quit_CH")
-                //                         .setDisabled(true)
+            //                 // EDIT THE INITIAL TICKET MESSAGE TO DISABLE BUTTON
+            //             // GRAB TICKET CHANNEL
+            //             initialChMsg = client.channels.cache.find(ch => ch.name === `closed-${dmUser.username.toLowerCase()}`)
+            //                 .then(ch => {
+            //                     // GRABBING THE INITIAL MESSAGE FROM TICKET CHANNEL
+            //                     msg = ch.messages.fetch(dbTicketData.TICKETCH1_MSG_ID)
 
-                //                     // BUTTON ROW
-                //                     let QuitButtonModBtn = new MessageActionRow()
-                //                         .addComponents(
-                //                             QuitButton
-                //                         );
+            //                     // CREATE INTRO EMBED FOR ADMIN/MOD TICKET CHANNEL
+            //                     let newTicketEditedEmbed = new discord.MessageEmbed()
+            //                         .setColor(config.embedGreen)
+            //                         .setTitle(`**Verification Ticket Closed**`)
+            //                         .addField(`User:`, `${dmUser}`, true)
+            //                         .addField(`User Tag:`, `${dmUser.tag}`, true)
+            //                         .addField(`User ID:`, `${dmUser.id}`, true)
+            //                         .setDescription(`*This ticket has been closed because the user has left the server.*`)
 
-                //                     // EDITING THE INITIAL DM PROMPT TO DISABLE BUTTONS
-                //                     msg.edit({ embeds: [newTicketEditedEmbed], components: [QuitButtonModBtn] })
-                //                         .catch(err => console.log(err))
-                //                 })
-                //     })
-                //     .catch(err => console.log(err))
-            })
+            //                     let QuitButton = new MessageButton()
+            //                         .setLabel("End Verification")
+            //                         .setStyle("DANGER")
+            //                         .setCustomId("quit_CH")
+            //                         .setDisabled(true)
+
+            //                     // BUTTON ROW
+            //                     let QuitButtonModBtn = new MessageActionRow()
+            //                         .addComponents(
+            //                             QuitButton
+            //                         );
+
+            //                     // EDITING THE INITIAL DM PROMPT TO DISABLE BUTTONS
+            //                     msg.edit({ embeds: [newTicketEditedEmbed], components: [QuitButtonModBtn] })
+            //                         .catch(err => console.log(err))
+            //                 })
+            //     })
+            //     .catch(err => console.log(err))
         }
 	},
 };
