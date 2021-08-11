926fb787a4d2e01be4ab1b943d24c37b0bf76536
Liam Chambers <leemleem1234@outlook.com>
2021-08-11 07:42:49 -0400

    Fixing database insert

--- SLASHCOMMANDS/Admin/user.js
+++ SLASHCOMMANDS/Admin/user.js
@@ -647,22 +647,18 @@ module.exports = {
             // CREATE DATABASE ENTRY FOR THE ISSUED WARNING
             await infractionsSchema.findOneAndUpdate({
                 USER_ID: warnUser.id,
-                ACTIONSARRAY: [{
-                    ACTION: 'WARN',
-                    REASON: warnReason,
-                    STAFF_ID: interaction.user.id,
-                    INTERACTION_DATE: new moment(Date.now()).format('LLL'),
-                    CASE_NUM: caseCounter
-                }]
+                ACTION: 'WARN',
+                REASON: warnReason,
+                STAFF_ID: interaction.user.id,
+                INTERACTION_DATE: new moment(Date.now()).format('LLL'),
+                CASE_NUM: caseCounter
             },{
                 USER_ID: warnUser.id,
-                ACTIONSARRAY: [{
-                    ACTION: 'WARN',
-                    REASON: warnReason,
-                    STAFF_ID: interaction.user.id,
-                    INTERACTION_DATE: new moment(Date.now()).format('LLL'),
-                    CASE_NUM: caseCounter
-                }]
+                ACTION: 'WARN',
+                REASON: warnReason,
+                STAFF_ID: interaction.user.id,
+                INTERACTION_DATE: new moment(Date.now()).format('LLL'),
+                CASE_NUM: caseCounter
             },{
                 upsert: true
             }).exec();
