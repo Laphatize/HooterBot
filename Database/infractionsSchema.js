8b40b760615a48fe2c2970f26036ec66cace7405
Liam Chambers <leemleem1234@outlook.com>
2021-08-11 07:40:22 -0400

    Going back to original schema setup

--- Database/infractionsSchema.js
+++ Database/infractionsSchema.js
@@ -2,16 +2,12 @@ const mongoose = require ('mongoose')
 
 module.exports = mongoose.model("Infractions", new mongoose.Schema({
     USER_ID: {type: String, required:true},
-    ACTIONSARRAY: [],
+    ACTION: {type: String, required:true},
+    REASON: {type: String, required:true},
+    STAFF_ID: {type: String, required:true},
+    DATE: {type: String, required:true},
+    CASE_NUM: {type: Number, required:true},
 },{
     timestamps: true,
     versionKey: false,
 }))
-
-
-// ACTIONSARRAY STRUCTURE:
-// ACTION: {type: String, required:true},
-// REASON: {type: String, required:true},
-// STAFF_ID: {type: String, required:true},
-// DATE: {type: String, required:true},
-// CASE_NUM: {type: Number, required:true},
