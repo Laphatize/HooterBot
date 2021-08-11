4e82c2116091034919fd7c587148444f5bc301cf
Liam Chambers <leemleem1234@outlook.com>
2021-08-11 00:47:57 -0400

    improved voice logging

--- botEvents/voiceStateUpdate.js
+++ botEvents/voiceStateUpdate.js
@@ -10,26 +10,26 @@ module.exports = {
 
 
         // JOIN VOICE CHANNEL
-        if(oldState.channel == 'null' && newState.channel !== 'null' ) {
+        if(oldState.channel == null && newState.channel !== null ) {
             let logEmbed = new discord.MessageEmbed()
                 .setColor(config.embedGrey)
-                .setTitle(`${newState.member.username} joined ${newState.channel.name}`)
+                .setTitle(`${newState.member.user.username} joined ${newState.channel.name}`)
                 .setDescription(`**Session ID:** ${newState.sessionId}`)
                 .setTimestamp()
-                .setFooter(`User ID: ${newState.member.id}`)
+                .setFooter(`User ID: ${newState.member.user.id}`)
 
             // LOG ENTRY
             modLogChannel.send({embeds: [logEmbed]})
         }
 
         // LEAVE VOICE CHANNEL
-        if(oldState.channel !== 'null' && newState.channel == 'null' ) {
+        if(oldState.channel !== null && newState.channel == null ) {
             let logEmbed = new discord.MessageEmbed()
                 .setColor(config.embedGrey)
-                .setTitle(`${newState.member.username} left ${newState.channel.name}`)
+                .setTitle(`${newState.member.user.username} left ${newState.channel.name}`)
                 .setDescription(`**Session ID:** ${newState.sessionId}`)
                 .setTimestamp()
-                .setFooter(`User ID: ${newState.member.id}`)
+                .setFooter(`User ID: ${newState.member.user.id}`)
 
             // LOG ENTRY
             modLogChannel.send({embeds: [logEmbed]})
@@ -38,15 +38,16 @@ module.exports = {
 
 
         // CHANGE VOICE CHANNEL
-        if(oldState.channel !== 'null' && newState.channel !== 'null' ) {
+        if(oldState.channel !== null && newState.channel !== null ) {
             // LOG EMBED
             let logEmbed = new discord.MessageEmbed()
                 .setColor(config.embedOrange)
-                .setTitle(`${newState.member.username} changed channels`)
-                .addField(`Old Channel:`, `${oldState.channel}\n**Session:** ${oldState.sessionId}`)
+                .setTitle(`${newState.member.user.username} changed channels`)
+                .addField(`Old Channel:`, `${oldState.channel}\n**Session:** ${oldState.sessionId}`, true)
                 .addField(`\u200b`, `🡲`, true)
-                .addField(`New Channel:`, `${newState.channel}\n**Session:** ${newState.sessionId}`)
+                .addField(`New Channel:`, `${newState.channel}\n**Session:** ${newState.sessionId}`, true)
                 .setTimestamp()
+                .setFooter(`User ID: ${newState.member.user.id}`)
 
             // LOG ENTRY
             modLogChannel.send({embeds: [logEmbed]})
