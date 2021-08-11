e485dcbf6a3c856e85e957622b6b7d7692fce20c
Liam Chambers <leemleem1234@outlook.com>
2021-08-11 03:58:57 -0400

    Removing username field from schema

--- Database/infractionsSchema.js
+++ Database/infractionsSchema.js
@@ -2,7 +2,6 @@ const mongoose = require ('mongoose')
 
 module.exports = mongoose.model("Infractions", new mongoose.Schema({
     USER_ID: {type: String, required:true},
-    USER_NAME: {type: String, required:true},
     ACTION: {type: String, required:true},
     REASON: {type: String, required:true},
     STAFF_ID: {type: String, required:true},
