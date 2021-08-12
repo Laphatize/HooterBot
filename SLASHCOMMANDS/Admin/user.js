7a875efc2a5596160ae20e981b2800a9e9d47173
Liam Chambers <leemleem1234@outlook.com>
2021-08-11 21:27:00 -0400

    Logging for role removal

--- SLASHCOMMANDS/Admin/user.js
+++ SLASHCOMMANDS/Admin/user.js
@@ -837,8 +837,13 @@ module.exports = {
             // FETCHING GUILD MEMBER
             interaction.guild.members.fetch(unmuteUser.id)
                 .then(member => {
+
+                    let hasMutedRole = member.roles.cache.has(interaction.guild.roles.cache.find(role => role.name == 'Muted :('))
+
+                    console.log(`hasMutedRole = ${hasMutedRole }`)
+
                     // USER IS NOT MUTED
-                    if(!member.roles.cache.has(role => role.name == 'Muted :(')) {
+                    if(!hasMutedRole) {
                         // GENERATE ERROR EMBED
                         let notMutedEmbed = new discord.MessageEmbed()
                             .setColor(config.embedRed)
