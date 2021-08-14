2de07acd2982ac3466f5c28a49a0e0e51ba36ee1
Liam Chambers <leemleem1234@outlook.com>
2021-08-14 00:43:10 -0400

    Trying to fix ping option

--- SLASHCOMMANDS/Admin/verif.js
+++ SLASHCOMMANDS/Admin/verif.js
@@ -510,7 +510,9 @@ module.exports = {
                     let adminRole = interaction.guild.roles.cache.find((role) => role.name.toLowerCase() == 'admin');
                     
                     // SENDING MESSAGE
-                    return interaction.reply({ embeds: [modAdminMsgEmbed], content: `${adminRole}s!` })
+                    interaction.reply({ embeds: [modAdminMsgEmbed] })
+
+                    return interaction.followUp({ content: `${adminRole}s!` })
                 }
                 // PING ADMINS AND MODS
                 if(pmgPing == 'ADMINS_MODS') {
@@ -520,7 +522,9 @@ module.exports = {
                     let adminRole = interaction.guild.roles.cache.find((role) => role.name.toLowerCase() == 'admin');
                     
                     // SENDING MESSAGE
-                    return interaction.reply({ embeds: [modAdminMsgEmbed], content: `${adminRole}s & ${modRole}s!` })
+                    interaction.reply({ embeds: [modAdminMsgEmbed] })
+
+                    return interaction.followUp({ content: `${adminRole}s & ${modRole}s!` })
                 }
                 else {
                     return interaction.reply({ embeds: [modAdminMsgEmbed] })
