de4982783d7b1b2ecc21286e655ba40900649c26
Liam Chambers <leemleem1234@outlook.com>
2021-09-23 15:38:29 -0400

    pageArrays

--- SLASHCOMMANDS/Moderator/stringblacklist.js
+++ SLASHCOMMANDS/Moderator/stringblacklist.js
@@ -332,7 +332,7 @@ module.exports = {
                         return new discord.MessageEmbed()
                             .setTitle('Blacklist Terms')
                             .setColor(config.embedDarkBlue)
-                            .setDescription(`pageArrays[x] = ${arrays[x]}`)
+                            .setDescription(`pageArrays[x] = ${pageArrays[x]}`)
                     });
 
                     const prevBtn = new MessageButton()
