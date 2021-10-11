9023b0d01372f7c7e41a2a0b5d050fd7a1dc262a
Liam Chambers <leemleem1234@outlook.com>
2021-10-11 18:35:04 -0400

    Threadding update

--- botEvents/newThreadIdea.js
+++ botEvents/newThreadIdea.js
@@ -11,8 +11,8 @@ module.exports = {
             
             let testEmbed = new discord.MessageEmbed()
                 .setColor(config.embedBlurple)
-                .setTitle(`This channel is using ${config.emjThread} threads!`)
-                .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time.`)
+                .setTitle(`This channel is using ${config.emjThread} threads to organize conversations!`)
+                .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time by sending a new message.`)
                 .addField(`Want to follow a discussion?`, `Select a thread and click the "Join" button. You'll join the thread and get notifications for any new messages.`)
                 .addField(`Want to talk about a new topic?`, `Create a new thread using the ${config.emjAttachment} button. Make sure your thread title is descriptive so others know what your thread is about.`)
                 .setFooter(`Threads may be renamed or closed by the admins or mods per the rules [which we'll have to add].`)
@@ -27,6 +27,14 @@ module.exports = {
 	name: 'threadCreate',
 	async execute(thread, client) {
 
-        thread.parent.send({ content: "A new thread was created with this channel as the parent, right?" })
+        let threadChannelEmbed = new discord.MessageEmbed()
+            .setColor(config.embedBlurple)
+            .setTitle(`This channel is using ${config.emjThread} threads to organize conversations!`)
+            .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time by sending a new message.`)
+            .addField(`Want to follow a discussion?`, `Select a thread and click the "Join" button. You'll join the thread and get notifications for any new messages.`)
+            .addField(`Want to talk about a new topic?`, `Create a new thread using the ${config.emjAttachment} button. Make sure your thread title is descriptive so others know what your thread is about.`)
+            .setFooter(`Threads may be renamed or closed by the admins or mods per the rules [which we'll have to add].`)
+
+        return thread.parent.send({ embeds: [threadChannelEmbed ], content: `\`\`thread.parent.id = ${thread.parent.id}\`\`\n`})
 	},
 };
