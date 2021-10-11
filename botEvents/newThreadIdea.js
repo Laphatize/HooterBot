4b0a0644947dcd23df789edda5079acb8c73cd0b
Liam Chambers <leemleem1234@outlook.com>
2021-10-11 19:19:19 -0400

    Threading notice

--- botEvents/newThreadIdea.js
+++ botEvents/newThreadIdea.js
@@ -5,18 +5,12 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
 
-        if (message.content.includes('threadChannelTestEmbed')) {
-            
+        if (message.content.includes('$threadedChannelTestEmbed') && message.author.id === config.botAuthorId) {
             setTimeout(() => message.delete(), 0 );
-            
             let testEmbed = new discord.MessageEmbed()
-                .setColor(config.embedBlurple)
-                .setTitle(`This channel is using ${config.emjThread} threads to organize conversations!`)
-                .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time by sending a new message.`)
-                .addField(`Want to follow a discussion?`, `Select a thread and click the "Join" button. You'll join the thread and get notifications for any new messages.`)
-                .addField(`Want to talk about a new topic?`, `Create a new thread using the ${config.emjAttachment} button. Make sure your thread title is descriptive so others know what your thread is about.`)
-                .setFooter(`Threads may be renamed or closed by the admins or mods per the rules [which we'll have to add].`)
-
+                .setColor(config.embedRed)
+                .setTitle(`Head's Up!`)
+                .setDescription(`The Admins are cooking up some changes and updates to the server and it will affect this channel. This channel will be temporarily offline tomorrow to implement these changes. Once that happens, consider using <#829409161581821997> or <#829409161581822000> until we restore the channel.\n\nThanks for understanding! `)
             return message.channel.send({ embeds: [testEmbed ] })
         }
     }
@@ -27,14 +21,19 @@ module.exports = {
 	name: 'threadCreate',
 	async execute(thread, client) {
 
-        let threadChannelEmbed = new discord.MessageEmbed()
-            .setColor(config.embedBlurple)
-            .setTitle(`This channel is using ${config.emjThread} threads to organize conversations!`)
-            .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time by sending a new message.`)
-            .addField(`Want to follow a discussion?`, `Select a thread and click the "Join" button. You'll join the thread and get notifications for any new messages.`)
-            .addField(`Want to talk about a new topic?`, `Create a new thread using the ${config.emjAttachment} button. Make sure your thread title is descriptive so others know what your thread is about.`)
-            .setFooter(`Threads may be renamed or closed by the admins or mods per the rules [which we'll have to add].`)
+        // THREAD CHANNEL ARRAY
+        if(
+            thread.parent.id === 897145655145930842
+        ){
+            let threadChannelEmbed = new discord.MessageEmbed()
+                .setColor(config.embedBlurple)
+                .setTitle(`This channel is using ${config.emjThread} threads to organize conversations!`)
+                .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time by sending a new message.`)
+                .addField(`Want to follow a discussion?`, `Select a thread and click the "Join" button. You'll join the thread and get notifications for any new messages.`)
+                .addField(`Want to talk about a new topic?`, `Create a new thread using the ${config.emjAttachment} button. Make sure your thread title is descriptive so others know what your thread is about.`)
+                .setFooter(`Threads may be renamed or closed by the admins or mods per the <#829409161581821994>.`)
 
-        return thread.parent.send({ embeds: [threadChannelEmbed ], content: `\`\`thread.parent.id = ${thread.parent.id}\`\`\n`})
+            return thread.parent.send({ embeds: [threadChannelEmbed ]})
+        }
 	},
 };
