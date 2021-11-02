a7c0f2546313b59948b196da887112f214a6266d
Liam Chambers <leemleem1234@outlook.com>
2021-11-02 09:32:06 -0400

    School channels set for threads

--- botEvents/threadChannels.js
+++ botEvents/threadChannels.js
@@ -7,13 +7,26 @@ module.exports = {
 
         // THREAD CHANNEL ARRAY
         if(
-            thread.parent.id === '829706960403955724' // PROSPECTIVE STUDENTS
-         || thread.parent.id === '897290329617739776' // UNIVERSITY HOUSING
-
-
-
-
+         // SCHOOL & COLLEGE CHANNELS:
+            thread.parent.id === '829709899377410120' // ART-AND-ARCHITECTURE
+         || thread.parent.id === '905082995893993472' // FOX-BUSINESS
+         || thread.parent.id === '829710131801227284' // DENTISTRY
+         || thread.parent.id === '905082819091525673' // EDUCATION-AND-HUMAN-DEVELOPMENT
+         || thread.parent.id === '905082371886415912' // ENGINEERING
+         || thread.parent.id === '829710377951559700' // LAW
+         || thread.parent.id === '905082007615324252' // LIBERAL-ARTS
+         || thread.parent.id === '905081827436421160' // MEDIA-AND-COMMS
+         || thread.parent.id === '829710783058935882' // MEDICINE
+         || thread.parent.id === '905081404013023262' // MUSIC-AND-DANCE
+         || thread.parent.id === '829710975308791858' // PHARMACY
+         || thread.parent.id === '829711074600157244' // PODIATRIC-MEDICINE
+         || thread.parent.id === '829711135829786644' // PUBLIC HEALTH
+         || thread.parent.id === '829711245380157440' // SCHOOL OF SOCIAL WORK
+         || thread.parent.id === '829711350166323281' // SPORT-TOURISM-HOSPITALITY-MANAGEMENT
+         || thread.parent.id === '905080106333110312' // SCIENCE-AND-TECHNOLOGY
          || thread.parent.id === '829711412884406312' // THEATER-FILM-MEDIA-ARTS
+        
+         // OTHER THREAD CHANNELS
 
         ){
             // DELETING PAST HOOTERBOT MESSAGES IN THE CHANNEL
