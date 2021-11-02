2f86ed265bafd5b597302e790694b66d5a02d866
Liam Chambers <leemleem1234@outlook.com>
2021-11-02 11:59:52 -0400

    Footer fix

--- botEvents/threadChannels.js
+++ botEvents/threadChannels.js
@@ -41,7 +41,7 @@ module.exports = {
                 .addField(`Want to participate in a discussion?`, `Select a thread above and start talking! You can reopen archived threads at any time by sending a new message.`)
                 .addField(`Want to follow a discussion?`, `Select a thread and click the "Join" button. You'll join the thread and get notifications for any new messages.`)
                 .addField(`Want to talk about a new topic?`, `Create a new thread using the ${config.emjAttachment} button. Make sure your thread title is descriptive so others know what your thread is about.`)
-                .setFooter(`Threads may be renamed or closed by the admins or mods per the <#829409161581821994>.`)
+                .setFooter(`Threads may be renamed or closed by the admins or mods per the server rules.`)
 
             return thread.parent.send({ embeds: [threadChannelEmbed ]})
         }
