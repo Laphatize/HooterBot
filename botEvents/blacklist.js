09b02579462eba64ef31003b0e31a1d08faff09a
Liam Chambers <leemleem1234@outlook.com>
2021-11-05 10:56:25 -0400

    blacklist webhook testing

--- botEvents/blacklist.js
+++ botEvents/blacklist.js
@@ -7,24 +7,33 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
 
+		// DELETE INITIAL MESSAGE
+		setTimeout(() => message.delete(), 0 );
+
+
 		// TEST SERVER
 		if(message.guild.id === '530503548937699340') {
 			if(message.content.includes('blacklistthistermplease')) {
-				setTimeout(() => message.delete(), 0 );
 
-
-				const webhookClient = client.fetchWebhook(process.env.testServerWebhookID, process.env.testServerWebhookToken)
+				// FETCH WEBHOOK
+				client.fetchWebhook(process.env.testServerWebhookID, process.env.testServerWebhookToken)
 					.then(webhook => {
-						message.channel.send(`Obtained the following webhook: ${JSON.stringify(webhook)}`)
-						console.log(`WEBHOOK INFO: \n${JSON.stringify(webhook)}`)
+
+						// EDIT WEBHOOK DISPLAY TO APPEAR AS USER
+						webhook.edit({
+							name: message.author.username,
+							avatar: message.author.displayAvatarURL(),
+							channel: webhook.channelId,
+						})
+						.then(userWebhook => {
+							// REDACTING BLACKLISTED TERM(S)
+
+							// coming soon :)
+
+							// POSTING REDACTED MESSAGE VIA WEBHOOK
+							message.channel.send({ content: 'The original message with the redacted term would go here. :)' })
+						})
 					})
-
-
-				// channel.createWebhook(message.author.username, {
-				// 	avatar: message.author.displayAvatarURL({ dynamic:true }),
-				// })
-				// 	.then(webhook => console.log(`Created webhook ${webhook}`))
-				// 	.catch(console.error);
 			}
 		}
 
