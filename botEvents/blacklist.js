bbecd53f3274ec0db5b88f7ddd2249bb28913b29
Liam Chambers <leemleem1234@outlook.com>
2021-11-05 10:40:49 -0400

    blacklist webhook test

--- botEvents/blacklist.js
+++ botEvents/blacklist.js
@@ -1,13 +1,32 @@
 const discord = require('discord.js');
+const { WebhookClient }  = require('discord.js');
 const config = require('../config.json');
-const ticketSchema = require('../Database/ticketSchema');
-const modAppTicketSchema = require('../Database/modappSchema');
 
 
 module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
 
+		// TEST SERVER
+		if(message.guild.id === '530503548937699340') {
+			if(message.content.includes('blacklistthistermplease')) {
+				setTimeout(() => message.delete(), 0 );
+
+
+				const webhookClient = client.fetchWebhook(process.env.testServerWebhookID, process.env.testServerWebhookToken)
+					.then(webhook => message.channel.send(`Obtained the following webhook:\nName: ${webhook.name}\nGuild: ${webhook.sourceGuild.name}`))
+
+
+				// channel.createWebhook(message.author.username, {
+				// 	avatar: message.author.displayAvatarURL({ dynamic:true }),
+				// })
+				// 	.then(webhook => console.log(`Created webhook ${webhook}`))
+				// 	.catch(console.error);
+			}
+		}
+
+
+		// TEMPLE SERVER
 
     }
 }
