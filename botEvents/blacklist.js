99976a088ae7a8ff63ebf99ffd34c0a686e7a571
Liam Chambers <leemleem1234@outlook.com>
2021-11-05 15:29:05 -0400

    Testing message space removal

--- botEvents/blacklist.js
+++ botEvents/blacklist.js
@@ -7,6 +7,11 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
 
+		// STRIPPING MESSAGE OF SPACES
+		let msg = message.content.split(' ').join('');
+		console.log(`msg = ${msg}`)
+
+
 		// TEST SERVER
 		if(message.guild.id === '530503548937699340') {
 			if(message.content.includes('blacklistthistermplease')) {
@@ -17,27 +22,30 @@ module.exports = {
 				// FETCH WEBHOOK AND EDIT
 				client.fetchWebhook(process.env.testServerWebhookID, process.env.testServerWebhookToken)
 				.then(webhook => {
-					console.log(`webhook fetched`)
 					webhook.edit({
 						name: message.author.username,
 						avatar: message.author.displayAvatarURL(),
 						channel: message.channel.id,
 					})
 					.then(userWebhook => {
-						console.log(`webhook edited, posting`)
 						// REDACTING BLACKLISTED TERM(S)
 
 						// coming soon :)
 
 						// POSTING REDACTED MESSAGE VIA WEBHOOK
 						userWebhook.send({ content: 'The original message with the redacted term would go here. :)' })
+						.then(webhook => {
+							webhook.edit({
+								name: 'blacklist filter webhook',
+								avatar: 'https://lh3.googleusercontent.com/-JwjA8wq5m292e4FcVyfTS8nvgkltfnAE6aMet54OHweH48gbKJhiZ0kUM8wHpDdiiZr=s85',
+							})
+						})
 					})
 				})
 			}
 		}
-
-
-		// TEMPLE SERVER
-
     }
 }
+
+
+// FILTERING FUNCTION
