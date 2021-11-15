fd898d933c2f31adf138934d19a01c806c17a81a
Liam Chambers <leemleem1234@outlook.com>
2021-11-15 16:23:54 -0500

    deploy-commands

---
+++ deploy-commands.js
@@ -0,0 +1,19 @@
+            const commands = [];
+
+            for (const folder of fs.readdirSync(`../SLASHCOMMANDS/`)) {
+                const commandFiles = fs.readdirSync(`../SLASHCOMMANDS/${folder}`).filter(file => file.endsWith('.js'));
+            
+                for (const file of commandFiles) {
+                    const command = require(`../SLASHCOMMANDS/${folder}/${file}`);
+                    
+                    commands.push(command.data.toJSON());
+                }
+            }
+
+            const rest = new REST({ version: '9' }).setToken(process.env.HB_BOT_TOKEN);
+
+            rest.put(Routes.applicationCommands(config.botId), { body: commands })
+                .then(() => console.log('Successfully registered application commands.'))
+                .catch(error => {
+                    console.log(`\n\nSLASH COMMAND DEPLOYMENT ERROR\n ${error}`);
+                });
